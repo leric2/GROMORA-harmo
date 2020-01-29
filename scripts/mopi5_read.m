ca58d379941fbb681e5d678d03f34b06c4ee17e3
Eric <eric.sauvageat@iap.unibe.ch>
2020-01-29 16:55:57 +0100

    new reading scipt for mopi

---
+++ scripts/mopi5_read.m
@@ -0,0 +1,61 @@
+function x=mopi5_read(file, ffts_model)
+tic
+
+S  = {'USRP-A', 'USRP-B','U5303', 'AC240'}; % FFTS model 1 to 4
+FS = [200 20  3200 2000]; % sampling rates in MHz 
+
+x=iap_read_2019(file,1);
+
+
+K=[1 2 5 7]; % Generic MOPI software includes 8 optional FFTS. Only these 4 are used
+
+if ffts_model==0 return; end
+
+for i = ffts_model
+		
+	% vector with file pointers to the beginning of each spectrum
+	bos = getfield( x, sprintf('BinFilePointer_%d', K(i)) ); 
+	N   = length(bos); % number of cycles
+	
+	CHANNELS = getfield( x, sprintf('NrOfChannels_%d', K(i)) );  % Number of channels
+	if length(CHANNELS)==1
+		channels=CHANNELS;
+		missing_channels=[]; 
+	else
+		channels=max(CHANNELS);
+		missing_channels=find(CHANNELS<channels);
+	end
+	
+	
+	x.data = zeros(N,channels);
+	fid = fopen( [file '.bin'], 'r', 'b');
+	
+	h = waitbar(0,sprintf('Reading %s in %s.bin', S{i}, file));
+	for n=1:N
+ 		fseek(fid, bos(n),-1);
+		x.data(n,:) = fread(fid, channels, 'float32');
+		waitbar(n/N,h)
+	end
+	close (h);
+	
+	% mark cycles with missing channels as NaN
+	x.data(missing_channels, :)=NaN; 
+	
+	% generate Frequency axis 
+	if i<3
+		x.f = FS(i)/2 * [-1:2/channels:1-2/channels]; % I/Q processing in USRP
+	else
+		x.f = FS(i)/2 * [0:1/channels:1-1/channels]; % real data in AC240 and U5303
+	end
+	
+end
+fclose(fid);
+toc
+
+
+
+
+
+
+
+
