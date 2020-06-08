65cda700218a0d53a0a893ca584cc63c3eef26cb
franziska <franziska.schranz@students.unibe.ch>
2020-06-08 15:26:53 +0200

    Added MIAWARA-C to import_default_calibrationTool.m (not complete)

--- scripts/calibration/import_default_calibrationTool.m
+++ scripts/calibration/import_default_calibrationTool.m
@@ -37,17 +37,17 @@ calibrationTool.binaryType='ieee-be';
 calibrationTool.h=6.62606876e-34; % [J/s]
 calibrationTool.kb=1.38065e-23;    % [J/K]
 
-calibrationTool.hotTemperatureStdThreshold=0.05;
+%calibrationTool.hotTemperatureStdThreshold=0.05;
 
 % minimum number of indices (h-a-c) we want in a calibration cycle for it
 % to be valid
-calibrationTool.minNumberOfIndicePerCycle=5;
+%calibrationTool.minNumberOfIndicePerCycle=5;
 
-calibrationTool.numberOfAquisitionSpectraHot=60000;
-calibrationTool.numberOfAquisitionSpectraAntenna=120000;
-calibrationTool.numberOfAquisitionSpectraCold=120000;
+%calibrationTool.numberOfAquisitionSpectraHot=60000;
+%calibrationTool.numberOfAquisitionSpectraAntenna=120000;
+%calibrationTool.numberOfAquisitionSpectraCold=120000;
 
-calibrationTool.TSysThresh=50;
+%calibrationTool.TSysThresh=50;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Importing default parameters for each instruments
@@ -55,7 +55,7 @@ calibrationTool.TSysThresh=50;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % GROMOS
-switch string(instrumentName)
+switch instrumentName
     case 'GROMOS'
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % Meta data
@@ -309,7 +309,124 @@ switch string(instrumentName)
         
         calibrationTool.calibrate=@(rawSpectra,log,calibrationTool,TCold,calType) calibrate_mopi5(rawSpectra,log,calibrationTool,TCold,calType);
         calibrationTool.check_calibrated=@(log,calibrationTool,calibratedSpectra) check_calibrated_mopi5(log,calibrationTool,calibratedSpectra);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MIAWARA-C
+    case 'MIAWARA-C'
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Meta data
+        calibrationTool.dataLocation='NY.ALESUND';
+        calibrationTool.PI_NAME='Murk;Axel';
+        calibrationTool.PI_AFFILIATION='Universtiy of Bern;UBERN';
+        calibrationTool.PI_ADDRESS='Sidlerstrasse 5, University of Bern;3012 Bern;Switzerland';
+        calibrationTool.PI_EMAIL='axel.murk@iap.unibe.ch'; 
+        calibrationTool.dataSource='MWR.H2O_UBERN002';
+        
+        % Geolocation
+        calibrationTool.lon=11.92;
+        calibrationTool.lat=78.92;
+        calibrationTool.altitude=0;
+        calibrationTool.azimuthAngle=0;
+        
+        % Observation frequency
+%         calibrationTool.observationFreq=%;
+%         
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         % Spectrometer data:
+        calibrationTool.numberOfSpectrometer=1;
+        calibrationTool.spectrometer='AC240';
+%         
+%         calibrationTool.samplingRateFFTS=%2000;
+%         
+        calibrationTool.numberOfChannels=32768;
+%         
+%         % Local oscillators information
+%         calibrationTool.fLO1=%1.45875e11;
+%         calibrationTool.fLO2=%3.6e9;
+%         
+%         % This one should correspond to the DC channel
+%         calibrationTool.LOFreqTot=%calibrationTool.fLO1-calibrationTool.fLO2-0.5e9;
+%         
+%         %calibrationTool.DCChannel=16384;
+%         calibrationTool.instrumentBandwidth=%1e9;
+%         %calibrationTool.LOFreq=1.45875e11;
+%   
+%         
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         % Parameters for flagging
+%         calibrationTool.TSysCenterTh=%2750;
+%         calibrationTool.TSysThresh=%100;
+%         calibrationTool.stdTSysThresh=%8;
+%         
+%         calibrationTool.THotTh=%313.9;
+%         calibrationTool.THotAbsThresh=%2;
+%         
+%         
+%         calibrationTool.numberOfTippingCurveExpected=%48;
+%         calibrationTool.toleranceTippingCurves=%2;
+%         calibrationTool.elevationAngleAntenna=%40;
+%         calibrationTool.elevationAngleCold=%-84;
+%         % -84 before 2020 ?????
+%         %calibrationTool.elevationAngleCold=-84;
+%         calibrationTool.elevationAngleHot=%160;
+%         
+%         calibrationTool.elevationAngleTolerance=%5;
+%         % Considering the expected number of tipping curve:
+%         calibrationTool.numberOfCyclesExpected=%1500;
+%         calibrationTool.toleranceNumberCycles=%0.01*calibrationTool.numberOfCyclesExpected;
+%         calibrationTool.tippingSize=%27;
+% 
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         % Corrections
+%         calibrationTool.tWindow=%0.99;
+%         
+%         calibrationTool.checkLevel0=true;
+% 
+%         calibrationTool.flipped_spectra=%true;
+%         calibrationTool.flip_spectra=%@(rawSpectra) flip_spectra_gromos(rawSpectra);
+%         
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         % Log file from the instrument:
+%         calibrationTool.THotUnit='degreeC';
+%         % Function for the harmonization of the log
+        calibrationTool.harmonize_log=@(log) harmonize_log_miawarac(log);  
+%         
+%         calibrationTool.indiceCold=%0;
+%         calibrationTool.indiceAntenna=%1;
+%         calibrationTool.indiceHot=%2;
+%         
+%         % Calibration outlier management
+%         calibrationTool.threshNumRawSpectraHot=%0.05*calibrationTool.numberOfChannels;
+%         calibrationTool.threshNumRawSpectraCold=%0.05*calibrationTool.numberOfChannels;
+% paths
+        calibrationTool.rawFileFolder='/home/franziska/Documents/MW/play_MIA-C_calibration/';%['/mnt/instrumentdata/miawarac/' dateStr(1:4) '/'];
+        %calibrationTool.rawFileFolder=['/scratch/mopi_rawData/'];
+        calibrationTool.level1Folder='/home/franziska/Documents/MW/play_MIA-C_calibration/';
+        
+        calibrationTool.file=[calibrationTool.rawFileFolder,calibrationTool.instrumentName,'_', calibrationTool.dateStr(1:4) '_' calibrationTool.dateStr(6:7) '_' calibrationTool.dateStr(9:10)];
+        
+        calibrationTool.meteoFolder='/home/franziska/Documents/MW/play_MIA-C_calibration/';
+        calibrationTool.observationFreq=22;
+        
+        calibrationTool.calibrationTime=60;
+        
+        %calibrationTool.fLO1=1.49275e11;
+        %calibrationTool.fLO2=5.6e9;
+        %calibrationTool.fLO3=2e9;
+        
+        % This one should correspond to the DC channel
+        calibrationTool.LOFreqTot=1.10e11;
+        calibrationTool.DCChannel=1; %=Nchannel/2 ??
+        
+
+        %calibrationTool.read_level0=@(calibrationTool) MIAWARA-C_read(calibrationTool); 
+        
+        calibrationTool.filenameLevel1a=['/home/franziska/Documents/MW/play_MIA-C_calibration/MIAWARA-C_level1a_' calibrationTool.spectrometer '_' calibrationTool.dateStr '.nc'];
+        
+        calibrationTool.calibrate=@(rawSpectra,log,calibrationTool,TCold,calType) calibrate_MIAWARA-C(rawSpectra,log,calibrationTool,TCold,calType);
+        calibrationTool.check_calibrated=@(log,calibrationTool,calibratedSpectra) check_calibrated_MIAWARA-C(log,calibrationTool,calibratedSpectra);
 end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Parameter varying with time for the instruments:
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -317,7 +434,7 @@ end
 % Conversion of datestr to datenum:
 timeNumber=datenum(str2num(dateStr(1:4)),str2num(dateStr(6:7)),str2num(dateStr(9:10)));
 
-switch string(instrumentName)
+switch instrumentName
     case 'GROMOS'
         % ...
     case 'SOMORA'
