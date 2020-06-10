3381a10d1443d3fb639069726a2b57a9de708ebf
Eric <eric.sauvageat@iap.unibe.ch>
2020-06-10 17:44:03 +0200

    apriori ECMWF oper and CIRA

--- scripts/retrieval/apriori_data_GROSOM.py
+++ scripts/retrieval/apriori_data_GROSOM.py
@@ -12,7 +12,7 @@ Using the Atmosphere Class to define a
 Including : 
     * a-priori data
 """
-
+import os
 import numpy as np
 import xarray as xr
 import pandas as pd
@@ -22,6 +22,9 @@ import matplotlib.pyplot as plt
 from retrievals import arts
 from retrievals.data.ecmwf import ECMWFLocationFileStore
 from retrievals.data.ecmwf import levels
+from retrievals.data import p_interpolate
+
+from typhon.arts.xml import load
 
 class APrioriDataGROSOM(arts.Atmosphere):
     '''
@@ -44,18 +47,27 @@ def extract_ecmwf_ds(ECMWF_store_path, t1, t2):
     )
     return ecmwf_ds
 
+def plot_apriori_cira86(retrieval_param):
+    lat = retrieval_param['lat']
+    month = pd.to_datetime(retrieval_param['time']).month
+
+    cira86 = read_cira86_monthly(retrieval_param['cira86_path'], month, lat)
+
+    plot_cira86_profile(cira86)
+    pass 
+
 def get_apriori_fascod(retrieval_param):
     fascod_atm = arts.Atmosphere.from_arts_xml(retrieval_param['prefix_atm'])
     return fascod_atm
 
 def get_apriori_atmosphere(retrieval_param):
     '''
-    Defining a-priori atmosphere from ECMWF operationnal dataset
+    Defining a-priori atmosphere from ECMWF operationnal dataset and Fascod clim
 
     First, we initiate an Atmosphere from Fascod climatology (not needed) before
     updating the required fields from ECMWF operationnal dataset.
 
-    TO DO: add the CIRA86 cimatology, otherwise the ecmwf data do not go high enough.
+    NOT WORKING YET
     '''
     
     ECMWF_store_path = retrieval_param['ecmwf_store_path']
@@ -76,3 +88,210 @@ def get_apriori_atmosphere(retrieval_param):
         )
     
     return atm
+
+def get_apriori_atmosphere_ecmwf_cira86(retrieval_param):
+    '''
+    Defining a-priori atmosphere from ECMWF operationnal dataset and CIRA86 clim
+
+    First, we initiate an Atmosphere from Fascod climatology (not needed) before
+    updating the required fields from ECMWF operationnal dataset.
+
+    TO DO: add the CIRA86 cimatology, otherwise the ecmwf data do not go high enough.
+    '''
+    
+    # Read EACMWF data (oper for now)
+    ECMWF_store_path = retrieval_param['ecmwf_store_path']
+    ds_ecmwf = extract_ecmwf_ds(ECMWF_store_path, t1 = '2019-04-16 03:30', t2 = '2019-04-16 16:45')
+    
+    # Reading CIRA86
+    lat = retrieval_param['lat']
+    month = pd.to_datetime(retrieval_param['time']).month
+
+    cira86 = read_cira86_monthly(retrieval_param['cira86_path'], month, lat)
+
+    #plot_ecmwf_cira86_profile(ds_ecmwf, cira86)
+
+    # Merging ecmwf and CIRA86
+    ds_ptz = merge_ecmwf_cira86(ds_ecmwf, cira86)
+
+    
+    plot_apriori_ptz(ds_ptz)
+
+    '''
+    At this point we have 2 choice:
+    1. Create atm using the fascod and overwrite our variable from ds_ptz
+
+    2. Create atm using "from_dataset". 
+    In this case:
+    The dataset must have the coordinates `pressure`, `lat`, `lon` and can have
+    variables `t`, `z` and absorbing species like `o3` (all lowercase).
+    --> It means we have to expand_dims at that point to use this method.
+    --> It means also that we need apriori values for each of the species in 
+    abs_species ......
+
+    '''
+
+    #ds_ptz = ds_ptz.expand_dims(dim=['lat','lon'])
+    # atm = arts.Atmosphere.from_dataset(ds_ptz)
+    atm = arts.Atmosphere.from_arts_xml(retrieval_param['prefix_atm'])
+
+    # Temperature
+    atm.set_t_field(ds_ptz['p'].values, ds_ptz['t'].values)
+
+    # z field
+    atm.set_z_field(ds_ptz["p"].values, ds_ptz["z"].values)
+    
+    # Ozone
+    atm.set_vmr_field(
+        "O3", ds_ecmwf["pressure"].values, ds_ecmwf['ozone_mass_mixing_ratio'].values
+        )
+
+    # Water vapor
+    atm.set_vmr_field(
+        "H2O", ds_ecmwf["pressure"].values, ds_ecmwf['specific_humidity'].values
+        )
+    
+    return atm
+
+def merge_ecmwf_cira86(ds_ecmwf, cira86):
+    '''
+    Merging profile from ECMWF oper and CIRA86 monthly climatology.
+
+    Start with a very simple scheme, we just take CIRA86 values when we reach the top
+    of the ECMWF ones.
+
+    Parameters
+        ----------
+        ds_ecmwf : xarray.Dataset
+            
+        cira86 : xarray.Dataset
+    
+    Returns
+        -------
+        TYPE
+            DESCRIPTION.
+    '''
+
+    upper_p_grid = cira86.Pressure.data[cira86.Pressure < np.min(ds_ecmwf.pressure.data)]
+    #upper_t = cira86.temperature.data[cira86.Pressure < np.min(ds_ecmwf.pressure.data)]
+    
+    upper_cira86_ds = cira86.sel(Pressure = upper_p_grid)
+
+    p_grid = np.hstack((ds_ecmwf.pressure.data, upper_p_grid))
+
+    #ecmwf_t_i = p_interpolate(
+    #    p_grid, ds_ecmwf.pressure.data, ds_ecmwf.temperature.data, fill=np.nan
+    #)
+
+    temperature = np.hstack((ds_ecmwf.temperature.data, upper_cira86_ds.temperature.data))
+
+    # interpolate CIRA86 altitude on p_grid
+    cira86_alt_i = p_interpolate(
+        p_grid, cira86.Pressure.data, cira86.altitude.data, fill = np.nan
+    )
+
+    ds_merged = xr.Dataset({'t': ('p', temperature),
+                            'z': ('p', cira86_alt_i)},
+                            coords = {
+                                'p' : ('p', p_grid),
+                            }
+    )
+
+    return ds_merged
+
+def read_cira86_monthly(cira86_datapath, month = None, latitude = None):
+    '''
+    Extract profile from the CIRA86 monthly climatology.
+
+    For now, works with the ones saved in arts-data saved as .xml files:
+    2 files per month in the form:
+    cira86_month*.t.xml -> temperature data, zonally averaged
+    cira86_month*.z.xml -> geometric altitude
+
+
+    Parameters
+        ----------
+        month : INT
+            
+        latitude : INT
+    
+    Returns
+        -------
+        TYPE
+            DESCRIPTION.
+    '''
+    
+    cira86_filename_t = 'cira86_month{}.t.xml'.format(month)
+    cira86_filename_z = 'cira86_month{}.z.xml'.format(month)
+
+    cira86_t = load(os.path.join(cira86_datapath, cira86_filename_t)).to_xarray()
+    cira86_z = load(os.path.join(cira86_datapath, cira86_filename_z)).to_xarray()
+
+    # merge the 2 ds
+    cira86 = xr.merge([cira86_z.rename('altitude'),cira86_t.rename('temperature')], compat='equals', join = 'inner')
+
+    # Select the right latitude
+    cira86_lat = np.round(latitude, -1)
+
+    if cira86_lat not in cira86_t.Latitude:
+        raise ValueError('No valid latitude')
+
+    return cira86.sel(Latitude = cira86_lat, Longitude = 0)
+
+def plot_ecmwf_cira86_profile(ds_ecmwf, cira86):
+    fig, axs = plt.subplots(1,2, sharex= True, sharey = True)
+    axs[0].plot(cira86.temperature, cira86.Pressure, label='Temperature')
+    axs[0].invert_yaxis()
+    axs[0].set_yscale('log')
+    axs[0].set_xlabel('T [K]')
+    axs[0].set_ylabel('$P$ [Pa]')
+
+    axs[1].plot(ds_ecmwf.temperature, ds_ecmwf.pressure, label='Temperature')
+    #axs[1].invert_yaxis()
+    axs[1].set_yscale('log')
+    axs[1].set_xlabel('T [K]')
+    axs[1].set_ylabel('$P$ [Pa]')
+
+    #fig.suptitle('CIRA86 Temperature profile')
+    fig.tight_layout()
+
+    fig.show()
+    pass
+
+def plot_apriori_ptz(ds_ptz):
+    fig, axs = plt.subplots(1,2, sharex= True)
+    axs[0].plot(ds_ptz.t, ds_ptz.p)
+    axs[0].invert_yaxis()
+    axs[0].set_yscale('log')
+    axs[0].set_xlabel('T [K]')
+    axs[0].set_ylabel('$P$ [Pa]')
+
+    axs[1].plot(ds_ptz.t, ds_ptz.z/1e3)
+    #axs[1].invert_yaxis()
+    #axs[1].set_yscale('log')
+    axs[1].set_xlabel('T [K]')
+    axs[1].set_ylabel('$Z$ [km]')
+
+    #fig.suptitle('CIRA86 Temperature profile')
+    fig.tight_layout()
+
+    fig.show()
+    pass
+
+def plot_cira86_profile(cira86):
+    fig, axs = plt.subplots(1,2)
+    axs[0].plot(cira86.temperature, cira86.Pressure, label='Temperature')
+    axs[0].invert_yaxis()
+    axs[0].set_yscale('log')
+    axs[0].set_xlabel('T [K]')
+    axs[0].set_ylabel('$P$ [Pa]')
+
+    axs[1].plot(cira86.temperature, cira86.altitude, label='Temperature')
+    axs[1].set_xlabel('T [K]')
+    axs[1].set_ylabel('$Z$ [m]')
+
+    fig.suptitle('CIRA86 Temperature profile')
+    fig.tight_layout()
+
+    fig.show()
+    pass
--- scripts/retrieval/testOEM_GROSOM.py
+++ scripts/retrieval/testOEM_GROSOM.py
@@ -6,276 +6,279 @@ Inspired from Jonas, this is a script to test the OEM retrievals for GROMOS.
 
 Ozone retrieval only on tropospheric corrected spectra.
 """
+import retrieval_module
+import apriori_data_GROSOM
+import data_GROSOM
+from retrievals import covmat
+from retrievals import arts
+from retrievals.arts.atmosphere import p2z_simple, z2p_simple
+from typhon.arts.workspace import arts_agenda
+from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 import warnings
 warnings.filterwarnings('ignore', message='numpy.dtype size changed')
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-from dotenv import load_dotenv
 
-#load_dotenv('/home/esauvageat/Documents/ARTS/arts-examples/.env')
+# load_dotenv('/home/esauvageat/Documents/ARTS/arts-examples/.env')
 load_dotenv('/home/eric/Documents/PhD/ARTS/arts-examples/.env.t490-arts2.3')
 ARTS_DATA_PATH = os.environ['ARTS_DATA_PATH']
 ARTS_BUILD_PATH = os.environ['ARTS_BUILD_PATH']
 ARTS_INCLUDE_PATH = os.environ['ARTS_INCLUDE_PATH']
 
-from typhon.arts.workspace import arts_agenda
+show_plots = True
+save_plots = False
+save_netcdf = False
 
-# to try new approach for z_grid
-from retrievals.arts.atmosphere import p2z_simple, z2p_simple
+'''
+Define to make the retrieval work as if launched from main_test.py,
+therefore using the retrieval_param structure even if not really needed...
+'''
+# For testing
+basename = "/home/eric/Documents/PhD/GROSOM/Level1/"
+level2_data_folder = "/home/eric/Documents/PhD/GROSOM/Level2/"
 
-from retrievals import arts
-from retrievals import covmat
+# basename="/home/esauvageat/Documents/GROSOM/Analysis/Level1/GROMOS/"
+# level2_data_folder = "/home/esauvageat/Documents/GROSOM/Analysis/Level2/"
 
-import data_GROSOM
-import apriori_data_GROSOM
-import retrieval_module
+line_file = ARTS_DATA_PATH+"/spectroscopy/Perrin_newformat_speciessplit/O3-666.xml.gz"
+# line_file = ARTS_DATA_PATH+"/spectroscopy/Hitran/O3-666.xml.gz"
 
-def make_f_grid(retrieval_param):
-    '''
-    create simulation frequency grid
-    '''
-    n_f = retrieval_param["number_of_freq_points"]# Number of points
-    bw = 1.5e9  # Bandwidth
-    x = np.linspace(-0.5, 0.5, n_f)
-    f_grid = x ** 3 + x / 10
-    f_grid = f_grid * bw / (max(f_grid) - min(f_grid))
-    #f_grid = np.linspace(retrieval_param["f_min"]-10, retrieval_param["f_max"]+10, n_f)
-    return f_grid
+instrument_name = "GROMOS"
+filename = basename+"GROMOS_level1b_AC240_2019_04_16"
 
-def main(show_plots=False, save_plots=False, save_netcdf=False):
-    '''
-    Define to make the retrieval work as if launched from main_test.py, 
-    therefore using the retrieval_param structure even if not really needed...
-    '''
-    # For testing
-    basename="/home/eric/Documents/PhD/GROSOM/Level1/"
-    level2_data_folder = "/home/eric/Documents/PhD/GROSOM/Level2/"
-    
-    #basename="/home/esauvageat/Documents/GROSOM/Analysis/Level1/GROMOS/"
-    #level2_data_folder = "/home/esauvageat/Documents/GROSOM/Analysis/Level2/"
-    
-    line_file = ARTS_DATA_PATH+"/spectroscopy/Perrin_newformat_speciessplit/O3-666.xml.gz"
-    #line_file = ARTS_DATA_PATH+"/spectroscopy/Hitran/O3-666.xml.gz"
-    
-    instrument_name="GROMOS"
-    filename = basename+"GROMOS_level1b_AC240_2019_04_16"
-    
-    retrieval_param = dict()
-    retrieval_param["integration_cycle"] = 4
-    retrieval_param["plot_meteo_ds"] = True
-    retrieval_param["number_of_freq_points"] = 601
-    retrieval_param["surface_altitude"] = 461
+retrieval_param = dict()
+retrieval_param["integration_cycle"] = 4
+retrieval_param["plot_meteo_ds"] = True
+retrieval_param["number_of_freq_points"] = 601
 
-    retrieval_param["azimuth_angle"]=32
-    retrieval_param["observation_altitude"] =  15e3
-    retrieval_param['obs_freq'] = 1.4217504e11
-    retrieval_param['line_file'] = line_file
-    
-    retrieval_param['boxcar_size'] = 128
-    
-    # known bad channel for this instrument (in the form of a numpy array):
-    retrieval_param['bad_channels'] = np.arange(0,104)
-    
-    retrieval_param['Tb_max'] = 200
-    retrieval_param['Tb_min'] = 0
-    retrieval_param['boxcar_thresh'] = 7
-    
-    fascod_atmosphere = 'midlatitude-summer'
-    retrieval_param['prefix_atm'] = ARTS_DATA_PATH + "/planets/Earth/Fascod/{}/{}.".format(fascod_atmosphere,fascod_atmosphere)
-    
-    level1b_dataset, meteo_ds, global_attrs_level1b = data_GROSOM.read_level1b(filename)
 
-    retrieval_param = {**global_attrs_level1b, **retrieval_param} 
+retrieval_param["azimuth_angle"] = 32
+retrieval_param["observation_altitude"] = 15e3
+retrieval_param['obs_freq'] = 1.4217504e11
+retrieval_param['line_file'] = line_file
 
-    level1b_dataset = data_GROSOM.find_bad_channels(level1b_dataset,retrieval_param)
-   
-    # Extracting some parameters
-    f0 = retrieval_param['obs_freq']
-    cycle = retrieval_param["integration_cycle"] 
-    ds_freq = level1b_dataset.frequencies.values
-    ds_num_of_channel = len(ds_freq)
-    #ds_Tb = level1b_dataset.Tb[cycle].values
-    ds_Tb_corr = level1b_dataset.Tb_corr[cycle].values
-    
-    ds_bw = max(ds_freq) - min(ds_freq)
-    
-    ds_df = ds_bw/(ds_num_of_channel-1)
-    
-    retrieval_param["zenith_angle"]=level1b_dataset.meanAngleAntenna.values[cycle]
-    
-    retrieval_param["time"] = level1b_dataset.time[cycle].values
-    retrieval_param["lat"] = level1b_dataset.lat[cycle].values
-    retrieval_param["lon"] = level1b_dataset.lon[cycle].values
-    
-    retrieval_param["f_max"] = max(ds_freq)
-    retrieval_param["f_min"] = min(ds_freq)
-    retrieval_param["bandwidth"] = max(ds_freq) - min(ds_freq)
+retrieval_param['boxcar_size'] = 128
 
-    ac = arts.ArtsController()
-    ac.setup(atmosphere_dim=1, iy_unit='RJBT', ppath_lmax=-1, stokes_dim=1)
+# known bad channel for this instrument (in the form of a numpy array):
+retrieval_param['bad_channels'] = np.arange(0, 104)
 
-    # Simulation grids
-    # try with
-    #z_bottom = 400
-    #z_top = 90e3
-    #z_res = 1e3
-    #z_grid = np.arange(z_bottom, z_top + 2 * z_res, z_res)
-    #p_grid = z2p_simple(z_grid)
+retrieval_param['Tb_max'] = 200
+retrieval_param['Tb_min'] = 0
+retrieval_param['boxcar_thresh'] = 7
 
-    f_grid = make_f_grid(retrieval_param) + f0
-    p_grid = np.logspace(5, -1, 161)
-    ac.set_grids(f_grid, p_grid)
+fascod_atmosphere = 'midlatitude-summer'
+retrieval_param['prefix_atm'] = ARTS_DATA_PATH + \
+    "/planets/Earth/Fascod/{}/{}.".format(
+    fascod_atmosphere, fascod_atmosphere)
 
-    # altitude for the retrieval
-    ac.set_surface(retrieval_param["surface_altitude"])
-    
-    # Spectroscopy
-    ac.set_spectroscopy_from_file(
-        abs_lines_file = retrieval_param['line_file'],
-        abs_species = ["O3","H2O-PWR98", "O2-PWR98","N2-SelfContStandardType"],
-        format = 'Arts',
-        line_shape = ["VVH", 750e9],
-        )
+level1b_dataset, meteo_ds, global_attrs_level1b = data_GROSOM.read_level1b(
+    filename)
 
-    # Atmosphere (a priori)
-    #fascod_atmosphere = 'midlatitude-summer'
-    #prefix_atm = ARTS_DATA_PATH + "/planets/Earth/Fascod/{}/{}.".format(fascod_atmosphere,fascod_atmosphere)
-    
-    fascod_atm = apriori_data_GROSOM.get_apriori_fascod(retrieval_param)
+retrieval_param = {**global_attrs_level1b, **retrieval_param}
 
-    retrieval_param['ecmwf_store_path'] = '/home/eric/Documents/PhD/GROSOM/ECMWF'
-    
-    # Does not work now, need to add higher climatology
-    # ecmwf_atm = apriori_data_GROSOM.get_apriori_atmosphere(retrieval_param)
-    ac.set_atmosphere(fascod_atm)
+level1b_dataset = data_GROSOM.find_bad_channels(
+    level1b_dataset, retrieval_param)
 
-    # Apply HSE, only allowed after setup of the atmosphere, when z is already on simulation grid
-    #ac.apply_hse(100e2, 0.5)
+# Extracting some parameters
+f0 = retrieval_param['obs_freq']
+cycle = retrieval_param["integration_cycle"]
+ds_freq = level1b_dataset.frequencies.values
+ds_num_of_channel = len(ds_freq)
+# ds_Tb = level1b_dataset.Tb[cycle].values
+ds_Tb_corr = level1b_dataset.Tb_corr[cycle].values
 
-    # Sensor pos/los/time
-    obs = arts.Observation(
-        za = 40, 
-        aa = 32, 
-        lat = 46,
-        lon = 7,
-        alt = 15e3,
-        time = 12
-    )
-    
-    ac.set_observations([obs])
-    ac.set_y([ds_Tb_corr])
+ds_bw = max(ds_freq) - min(ds_freq)
 
-    # Defining our sensors
-    sensor = arts.SensorFFT(ds_freq, ds_df)
-    ac.set_sensor(sensor)
-    
-    # doing the checks
-    ac.checked_calc()
-    y_FM, = ac.y_calc()
+ds_df = ds_bw/(ds_num_of_channel-1)
 
-    # Setup the retrieval
-    p_ret_grid = np.logspace(5, -1, 81)
-    lat_ret_grid = np.array([retrieval_param["lat"]])
-    lon_ret_grid = np.array([retrieval_param["lon"]])
-    
-    sx_O3 = covmat.covmat_diagonal_sparse(1e-6 * np.ones_like(p_ret_grid))
-    #sx_H2O = covmat.covmat_diagonal_sparse(1e-6 * np.ones_like(p_ret_grid))
-    
-    # The different things we want to retrieve
-    ozone_ret = arts.AbsSpecies('O3', p_ret_grid, lat_ret_grid, lon_ret_grid, sx_O3, unit='vmr')
-    #h2o_ret = arts.AbsSpecies('H2O', p_ret_grid, lat_ret_grid, lon_ret_grid, sx_H2O, unit='vmr')
-    
-    #fshift_ret = arts.FreqShift(100e3, df=50e3)
-    #polyfit_ret = arts.Polyfit(poly_order=1, covmats=[np.array([[0.5]]), np.array([[1.4]])])
-    
-    # increase variance for spurious spectra by factor
-    retrieval_param['increased_var_factor'] = 100
-    factor = retrieval_param['increased_var_factor']
-    retrieval_param['unit_var_y']  = 2e-2
-    
-    y_var = retrieval_param['unit_var_y'] * np.ones_like(ds_freq)
-    
-    y_var[(level1b_dataset.good_channels[cycle].values==0)] = factor*retrieval_param['unit_var_y']
-    
-    #y_var = utils.var_allan(ds_Tb) * np.ones_like(ds_Tb)
+retrieval_param["zenith_angle"] = level1b_dataset.meanAngleAntenna.values[cycle]
 
-    ac.define_retrieval([ozone_ret], y_var)
-    #ac.define_retrieval([ozone_ret, fshift_ret, polyfit_ret], y_var)
-    
-    # Let a priori be off by 0.5 ppm (testing purpose)
-    #vmr_offset = -0.5e-6
-    #ac.ws.Tensor4AddScalar(ac.ws.vmr_field, ac.ws.vmr_field, vmr_offset)
-    
-    # Run retrieval (parameter taken from MOPI)
-    # SOMORA is using 'lm': Levenberg-Marquardt (LM) method
-    ac.oem(
-        method='lm',
-        max_iter=10,
-        stop_dx=0.01,
-        lm_ga_settings=[100.0, 3.0, 5.0, 10.0, 1.0, 10.0],
-        inversion_iterate_agenda=inversion_iterate_agenda,
-      )
+retrieval_param["time"] = level1b_dataset.time[cycle].values
+retrieval_param["lat"] = level1b_dataset.lat[cycle].values
+retrieval_param["lon"] = level1b_dataset.lon[cycle].values
 
-    if not ac.oem_converged:
-        print("OEM did not converge.")
-        print("OEM diagnostics: " + str(ac.oem_diagnostics))
-        for e in ac.oem_errors:
-            print("OEM error: " + e)
-            continue
-    
-    # Plots!
-    yf = ac.yf[0]
-    
-    fig, axs = plt.subplots(2, sharex=True)
-    axs[0].plot((ds_freq - f0) / 1e6, ds_Tb_corr, label='observed')
-    axs[0].plot((ds_freq - f0) / 1e6, yf, label='fitted')
-    #axs[0].plot((ds_freq - f0) / 1e6, bl, label='baseline')
-    axs[0].legend()
-    axs[1].plot((ds_freq - f0) / 1e6, ds_Tb_corr-yf, label='observed - computed')
-    axs[1].legend()
-    axs[1].set_xlabel('f - {:.3f} GHz [MHz]'.format(f0/1e9))
-    axs[0].set_ylabel('$T_B$ [K]')
-    axs[1].set_ylabel('$T_B$ [K]')
-    fig.tight_layout()
-    if save_plots:
-        fig.savefig('testOEM_spectrum_GROSOM.png')
-    if show_plots:
-        fig.show()
+retrieval_param["f_max"] = max(ds_freq)
+retrieval_param["f_min"] = min(ds_freq)
+retrieval_param["bandwidth"] = max(ds_freq) - min(ds_freq)
 
-    fig, axs = plt.subplots(2, 2, sharey=True)
+ac = arts.ArtsController()
+ac.setup(atmosphere_dim=1, iy_unit='RJBT', ppath_lmax=-1, stokes_dim=1)
 
-    axs[0][0].plot(ozone_ret.x * 1e6, ozone_ret.z_grid / 1e3, label='retrieved', marker='x')
-    axs[0][0].plot(ozone_ret.xa * 1e6, ozone_ret.z_grid / 1e3, label='apriori')
-    #axs[0][0].plot((ozone_ret.xa - vmr_offset) * 1e6, ozone_ret.z_grid / 1e3, label='true')
-    axs[0][0].set_xlabel('Ozone VMR [ppm]')
-    axs[0][0].set_ylabel('Altitude [km]')
-    axs[0][0].legend()
 
-    axs[0][1].plot(ozone_ret.mr, ozone_ret.z_grid / 1e3)
-    axs[0][1].set_xlabel('Measurement response')
+#TODO
+# Simulation grids
+# frequency grid
+n_f = retrieval_param["number_of_freq_points"]  # Number of points
+bw = 1.5e9  # Bandwidth
+x = np.linspace(-0.5, 0.5, n_f)
+f_grid = x ** 3 + x / 10
+f_grid = f_grid * bw / (max(f_grid) - min(f_grid))
 
-    axs[1][0].plot(ozone_ret.offset / 1e3, ozone_ret.z_grid / 1e3)
-    axs[1][0].set_xlabel('Offset [km]')
-    axs[1][0].set_ylabel('Altitude [km]')
+f_grid = f_grid + f0
 
-    for avk in ozone_ret.avkm:
-        if 0.8 <= np.sum(avk) <= 1.2:
-            axs[1][1].plot(avk, ozone_ret.z_grid / 1e3)
+# Pressure grid 
+z_bottom = 1000
+z_top = 95e3
+z_res = 1e3
+z_grid = np.arange(z_bottom, z_top + 2 * z_res, z_res)
+p_grid = z2p_simple(z_grid)
 
-    fig.tight_layout()
-    
-    if save_plots:
-        fig.savefig('test_oem_ozone.png')
-        print('\nSaved plots to TestOEM_*.png')
-    if show_plots:
-        fig.show()
+# p_grid = np.logspace(5, -1, 161)
+ac.set_grids(f_grid, p_grid)
 
-    #if save_netcdf:
-    #    ac.get_level2_xarray().to_netcdf('TestOEM_result.nc')
-    #    print('\nSaved results to TestOEM_result.nc')
-    
-    return True
+# altitude for the retrieval
+retrieval_param["surface_altitude"] = 1500
+ac.set_surface(retrieval_param["surface_altitude"])
+
+# Spectroscopy
+ac.set_spectroscopy_from_file(
+    abs_lines_file=retrieval_param['line_file'],
+    abs_species=["O3", "H2O-PWR98", "O2-PWR98", "N2-SelfContStandardType"],
+    format='Arts',
+    line_shape=["VVH", 750e9],
+)
+
+# Atmosphere (a priori)
+# fascod_atmosphere = 'midlatitude-summer'
+# prefix_atm = ARTS_DATA_PATH + "/planets/Earth/Fascod/{}/{}.".format(fascod_atmosphere,fascod_atmosphere)
+
+retrieval_param['cira86_path'] = os.path.join(
+    ARTS_DATA_PATH, 'planets/Earth/CIRA86/monthly')
+
+#apriori_data_GROSOM.plot_apriori_cira86(retrieval_param)
+
+#fascod_atm = apriori_data_GROSOM.get_apriori_fascod(retrieval_param)
+
+retrieval_param['ecmwf_store_path'] = '/home/eric/Documents/PhD/GROSOM/ECMWF'
+
+atm = apriori_data_GROSOM.get_apriori_atmosphere_ecmwf_cira86(
+    retrieval_param)
+# ecmwf_atm = apriori_data_GROSOM.get_apriori_atmosphere(retrieval_param)
+
+# Does not work now:
+# The grid vector *retrieval pressure grid* is not covered by the
+# corresponding atmospheric grid.
+ac.set_atmosphere(atm, vmr_zeropadding=True)
+
+# Apply HSE, only allowed after setup of the atmosphere, when z is already on simulation grid
+ac.apply_hse(100e2, 0.1)  # value taken from GROMOS retrieval
+
+# Sensor pos/los/time
+obs = arts.Observation(
+    za=40,
+    aa=32,
+    lat=46,
+    lon=7,
+    alt=15e3,
+    time=12
+)
+
+ac.set_observations([obs])
+ac.set_y([ds_Tb_corr])
+
+# Defining our sensors
+sensor = arts.SensorFFT(ds_freq, ds_df)
+ac.set_sensor(sensor)
+
+# doing the checks
+ac.checked_calc()
+y_FM = ac.y_calc()
+
+# Setup the retrieval #TODO
+p_ret_grid = np.logspace(5, -1, 81)
+
+lat_ret_grid = np.array([retrieval_param["lat"]])
+lon_ret_grid = np.array([retrieval_param["lon"]])
+
+sx_O3 = covmat.covmat_diagonal_sparse(1e-6 * np.ones_like(p_ret_grid))
+# sx_H2O = covmat.covmat_diagonal_sparse(1e-6 * np.ones_like(p_ret_grid))
+
+# The different things we want to retrieve
+ozone_ret = arts.AbsSpecies(
+    'O3', p_ret_grid, lat_ret_grid, lon_ret_grid, sx_O3, unit='vmr')
+# h2o_ret = arts.AbsSpecies('H2O', p_ret_grid, lat_ret_grid, lon_ret_grid, sx_H2O, unit='vmr')
+
+# fshift_ret = arts.FreqShift(100e3, df=50e3)
+# polyfit_ret = arts.Polyfit(poly_order=1, covmats=[np.array([[0.5]]), np.array([[1.4]])])
+
+# increase variance for spurious spectra by factor
+retrieval_param['increased_var_factor'] = 100
+factor = retrieval_param['increased_var_factor']
+retrieval_param['unit_var_y'] = 2e-2
+
+y_var = retrieval_param['unit_var_y'] * np.ones_like(ds_freq)
+
+y_var[(level1b_dataset.good_channels[cycle].values == 0)] = factor*retrieval_param['unit_var_y']
+
+# y_var = utils.var_allan(ds_Tb) * np.ones_like(ds_Tb)
+ac.define_retrieval([ozone_ret], y_var)
+# ac.define_retrieval([ozone_ret, fshift_ret, polyfit_ret], y_var)
+
+# Let a priori be off by 0.5 ppm (testing purpose)
+# vmr_offset = -0.5e-6
+# ac.ws.Tensor4AddScalar(ac.ws.vmr_field, ac.ws.vmr_field, vmr_offset)
+
+# Run retrieval (parameter taken from MOPI)
+# SOMORA is using 'lm': Levenberg-Marquardt (LM) method
+ac.oem(
+     method='lm',
+     max_iter=10,
+     stop_dx=0.01,
+     lm_ga_settings=[100.0, 3.0, 5.0, 10.0, 1.0, 10.0],
+     inversion_iterate_agenda=inversion_iterate_agenda,
+)
+
+if not ac.oem_converged:
+    print("OEM did not converge.")
+    print("OEM diagnostics: " + str(ac.oem_diagnostics))
+    for e in ac.oem_errors:
+        print("OEM error: " + e)
+        continue
+
+# Plots!
+yf = ac.yf[0]
+fig, axs = plt.subplots(2, sharex=True)
+axs[0].plot((ds_freq - f0) / 1e6, ds_Tb_corr, label='observed')
+axs[0].plot((ds_freq - f0) / 1e6, yf, label='fitted')
+# axs[0].plot((ds_freq - f0) / 1e6, bl, label='baseline')
+axs[0].legend()
+axs[1].plot((ds_freq - f0) / 1e6, ds_Tb_corr -
+            yf, label='observed - computed')
+axs[1].legend()
+axs[1].set_xlabel('f - {:.3f} GHz [MHz]'.format(f0/1e9))
+axs[0].set_ylabel('$T_B$ [K]')
+axs[1].set_ylabel('$T_B$ [K]')
+fig.tight_layout()
+if save_plots:
+    fig.savefig('testOEM_spectrum_GROSOM.png')
+if show_plots:
+    fig.show()
+fig, axs = plt.subplots(2, 2, sharey=True)
+axs[0][0].plot(ozone_ret.x * 1e6, ozone_ret.z_grid /
+               1e3, label='retrieved', marker='x')
+axs[0][0].plot(ozone_ret.xa * 1e6, ozone_ret.z_grid / 1e3, label='apriori')
+# axs[0][0].plot((ozone_ret.xa - vmr_offset) * 1e6, ozone_ret.z_grid / 1e3, label='true')
+axs[0][0].set_xlabel('Ozone VMR [ppm]')
+axs[0][0].set_ylabel('Altitude [km]')
+axs[0][0].legend()
+axs[0][1].plot(ozone_ret.mr, ozone_ret.z_grid / 1e3)
+axs[0][1].set_xlabel('Measurement response')
+axs[1][0].plot(ozone_ret.offset / 1e3, ozone_ret.z_grid / 1e3)
+axs[1][0].set_xlabel('Offset [km]')
+axs[1][0].set_ylabel('Altitude [km]')
+for avk in ozone_ret.avkm:
+    if 0.8 <= np.sum(avk) <= 1.2:
+        axs[1][1].plot(avk, ozone_ret.z_grid / 1e3)
+fig.tight_layout()
+if save_plots:
+    fig.savefig('test_oem_ozone.png')
+    print('\nSaved plots to TestOEM_*.png')
+if show_plots:
+    fig.show()
+# if save_netcdf:
+#    ac.get_level2_xarray().to_netcdf('TestOEM_result.nc')
+#    print('\nSaved results to TestOEM_result.nc')
 
 @arts_agenda
 def inversion_iterate_agenda(ws):
@@ -285,7 +288,7 @@ def inversion_iterate_agenda(ws):
     # Map x to ARTS' variables
     ws.x2artsAtmAndSurf()
     ws.x2artsSensor()
-    
+
     # To be safe, rerun some checks
     ws.atmfields_checkedCalc(negative_vmr_ok=1)
     ws.atmgeom_checkedCalc()
@@ -299,6 +302,3 @@ def inversion_iterate_agenda(ws):
     # This method takes cares of some "fixes" that are needed to get the Jacobian
     # right for iterative solutions. No need to call this WSM for linear inversions.
     ws.jacobianAdjustAndTransform()
-
-if __name__ == '__main__':
-    main(show_plots=True, save_plots=True, save_netcdf=False)
