2015106e2f890f923a2ca40672298e3094b4f91a
Eric <eric.sauvageat@iap.unibe.ch>
2020-07-15 19:05:22 +0200

    better plots

--- scripts/calibration/plot_integrated_spectra_generic.m
+++ scripts/calibration/plot_integrated_spectra_generic.m
@@ -2,17 +2,113 @@ function plot_integrated_spectra_generic(calibrationTool,integratedSpectra,lower
 % Just for a first look
 try   
     % plotting a spectra every numberOfSpectraToGroup measurements
-    TOD={};
+    %TOD={};
     N=24;
     fig=figure('visible','off');
+    %fig=figure();
     clf
     set(gcf, 'PaperPosition', [1 1 19 27.7])
     orient(fig,'landscape')
     %suptitle([calibrationTool.dateStr(1:4) '-' calibrationTool.dateStr(6:7) '-' calibrationTool.dateStr(9:10)])
     cm = colormap(parula(N));
-    TOD{1}=[num2str(0) ' h'];
+    %TOD{1}=[num2str(0) ' h'];
+    
+    ax = subplot(3,2,1);
+    grid on
+    yyaxis(ax(1),'left')
+    plot([integratedSpectra.dateTime],[integratedSpectra.TSys],'k');
     hold on
-    subplot(2,2,[1,3]); 
+    plot([integratedSpectra.dateTime], nanmedian([integratedSpectra.TSys])*[integratedSpectra.outlierCalib],'mx');
+    set(ax(1),'ylim',[nanmedian([integratedSpectra.TSys])-30,nanmedian([integratedSpectra.TSys])+20])
+    %set(ax(1),'xlim', [0,24])
+    set(ax(1),'YColor','k');
+    ylabel(ax(1),({'TN [K]'}))
+    
+    yyaxis(ax(1),'right')
+    plot([integratedSpectra.dateTime],[integratedSpectra.THot],'r');
+    set(ax(1),'ylim', [nanmedian([integratedSpectra.THot])-0.3,nanmedian([integratedSpectra.THot])+0.4])
+    %set(ax(1),'xlim', [0,24])
+    set(ax(1),'YColor','r');
+    ylabel(ax(1),({'THot [K]'}))
+    
+    
+    ax2 = subplot(3,2,2);
+    yyaxis(ax2(1),'left')
+    plot(ax2, [integratedSpectra.dateTime],[integratedSpectra.meanTb],'g');
+    hold on
+    plot([integratedSpectra.dateTime], nanmedian([integratedSpectra.meanTb])*[integratedSpectra.outlierCalib],'mx');
+   % set(ax(1),'ylim', [0,300])
+    set(ax2,'ylim', [0,nanmedian([integratedSpectra.meanTb])+100])
+    set(ax2,'YColor','g');
+    ylabel(ax2(1),({'Tb [K]'}))
+    
+    yyaxis(ax2(1),'right')
+    plot([integratedSpectra.dateTime],[integratedSpectra.meanAirTemperature ],'r');
+   % set(ax(1),'ylim', [0,300])
+    %set(ax(1),'xlim', [0,24])
+    set(ax2(1),'YColor','r');
+    ylabel(ax2(1),({'airT [K]'}))
+    grid on
+%     yyaxis(ax2(1),'right')
+%     plot([integratedSpectra.dateTime],[integratedSpectra.THot],'r');
+%     %set(ax(1),'xlim', [0,24])
+%     set(ax2(1),'YColor','r');
+%     ylabel(ax2(1),({'THot [K]'}))
+    
+    %ax3 = subplot(3,2,3);
+
+    
+%     yyaxis(ax3(1),'right')
+%     plot([integratedSpectra.dateTime],[integratedSpectra.meanAirPressure],'k');
+%     set(ax(1),'xlim', [0,24])
+%     set(ax3(1),'YColor','k');
+%     ylabel(ax3(1),({'airP [hPa]'}))
+
+    ax3 = subplot(3,2,3);
+    yyaxis(ax3(1),'left')
+    plot([integratedSpectra.dateTime],[integratedSpectra.numAntSpectra],'g');
+    hold on
+    plot([integratedSpectra.dateTime],[integratedSpectra.numColdSpectra],'b');
+    plot([integratedSpectra.dateTime],[integratedSpectra.numHotSpectra],'r');
+    %set(ax(1),'ylim', [0,300])
+    %set(ax(1),'xlim', [0,24])
+    set(ax3(1),'YColor','k');
+    ylabel(ax3(1),({'# of spectra [-]'}))
+    
+    yyaxis(ax3(1),'right')
+    plot([integratedSpectra.dateTime],[integratedSpectra.numberOfAveragedSpectra],'k');
+    set(ax3(1),'ylim', [-0.1,6.1])
+    set(ax3(1),'YColor','k');
+    ylabel(ax3(1),({'# cal cycle [-]'}))
+    grid on
+    
+    ax4 = subplot(3,2,4);
+    yyaxis(ax4(1),'left')
+    plot([integratedSpectra.dateTime],[integratedSpectra.rainAccumulation ],'k');
+   % set(ax(1),'ylim', [0,300])
+    %set(ax(1),'xlim', [0,24])
+    set(ax4(1),'YColor','k');
+    ylabel(ax4(1),({'precipitation [...]'}))
+    
+    yyaxis(ax4(1),'right')
+    plot([integratedSpectra.dateTime],100*[integratedSpectra.meanRelativeHumidity],'b');
+    set(ax4(1),'ylim', [0,100])
+    set(ax4(1),'YColor','b');
+    ylabel(ax4(1),({'RH [%]'}))
+    grid on
+
+%     for i=1:N
+%         %plot(integratedSpectra(i).if,integratedSpectra(i).TbWinCorr.*integratedSpectra(i).channelsQuality,'Color',cm(i,:));
+%         plot(integratedSpectra(i).dateTime,integratedSpectra(i).TSys);
+%         %xlabel('IF [MHz]')
+%         %ylabel('T_B [K]')
+%         
+%         %lim([lowerLim,upperLim])
+%         hold on
+%     end
+%    title('Corrected (windows), good channels')
+    
+    subplot(3,2,5); 
     for i=1:N
         plot(integratedSpectra(i).if,integratedSpectra(i).Tb,'Color',cm(i,:));
         %title([calibrationTool.dateStr(1:4) '-' calibrationTool.dateStr(6:7) '-' calibrationTool.dateStr(9:10)])
@@ -20,26 +116,15 @@ try
         ylabel('T_B [K]')
         
         ylim([lowerLim,upperLim])
-        TOD{i}=num2str(integratedSpectra(i).TOD);
+        ylim([nanmin([integratedSpectra.meanTb])-10,nanmax([integratedSpectra.meanTb])+20])
+        %TOD{i}=num2str(integratedSpectra(i).TOD);
         hold on
     end
     title('Non Corrected, all channels')
     grid on
-    legend(TOD,'Location','southoutside','NumColumns',4)
+    %legend(TOD,'Location','southoutside','NumColumns',3)
     
-    subplot(2,2,2); 
-    for i=1:N
-        plot(integratedSpectra(i).if,integratedSpectra(i).TbWinCorr.*integratedSpectra(i).channelsQuality,'Color',cm(i,:));
-        xlabel('IF [MHz]')
-        ylabel('T_B [K]')
-        
-        ylim([lowerLim,upperLim])
-        hold on
-    end
-    title('Corrected (windows), good channels')
-    grid on
-    
-    subplot(2,2,4);
+    subplot(3,2,6);
     for i=1:N
        
         plot(integratedSpectra(i).if,integratedSpectra(i).TbTroposphericWindowCorr.*integratedSpectra(i).channelsQuality,'Color',cm(i,:));
@@ -48,18 +133,19 @@ try
         ylabel('T_B [K]')
 
         %ylim([lowerLim,upperLim])
+        
         hold on
     end
-    title('Corrected (troposphere), good channels')
     grid on
-    
+    title('Corrected, good channels')
     
     % saveas(gcf,[retrievalTool.level1Folder 'calibratedHourlySpectra_' correctedSpectra.date],'jpg')
     print([calibrationTool.level1Folder calibrationTool.instrumentName '_integratedSpectra_' calibrationTool.spectrometer '_' calibrationTool.dateStr],'-dpdf','-fillpage')
+    %print(fig,[calibrationTool.level1Folder calibrationTool.instrumentName '_integratedSpectra_' calibrationTool.spectrometer '_' calibrationTool.dateStr],'-dpsc','-fillpage')
     close
 
 catch ME
-    warning(ME.identifier,'Problem Plotting')
+    warning(ME.identifier,'%s',['Plotting integration problem: ' ME.message])
 end
 
 end
--- scripts/calibration/plot_spectra_generic.m
+++ scripts/calibration/plot_spectra_generic.m
@@ -1,135 +1,146 @@
 function plot_spectra_generic(calibrationTool, drift, meteoData,calibratedSpectra,lowerLim,upperLim,N)
 % Just for a first look
 % plotting a spectra every N measurements 
-l=floor(linspace(1,length(calibratedSpectra),N));
-
-%TOD={};
-
-fig = figure('visible','off');
-%fig = figure();
-clf
-set(gcf, 'PaperPosition', [1 1 19 27.7])
-orient(fig,'landscape')
-
-if ~isempty(drift)
-    ax = subplot(3,2,1); plot(ax,drift.dateTime, drift.Tn, 'k'), hold on, plot(ax,drift.dateTime,drift.TSysLog,'y'), ylabel('Tn [K]') ,ylim([nanmedian(drift.Tn)-80,nanmedian(drift.Tn)+80])
-    plot(ax, [calibratedSpectra.meanAntTime], nanmedian(drift.Tn)*[calibratedSpectra.outlierCalib],'mx');
-
-    ax2 = subplot(3,2,2); plot(ax2, drift.dateTime, drift.Ta ,'g'), hold on,ylabel('Ta [K]'),ylim([0,nanmedian(drift.Ta)+120])
-    plot(ax2, [calibratedSpectra.meanAntTime], nanmedian(drift.Ta)*[calibratedSpectra.outlierCalib],'mx');
-    if ~isempty(drift.outlierCold)
-        plot(ax, drift.outlierCold,nanmedian(drift.Tn)-50,'bx')
-        plot(ax2, drift.outlierCold,10,'bx')
+try 
+    l=floor(linspace(1,length(calibratedSpectra),N));
+    
+    %TOD={};
+    
+    fig = figure('visible','off');
+    %fig = figure();
+    clf
+    set(gcf, 'PaperPosition', [1 1 19 27.7])
+    orient(fig,'landscape')
+    
+    limTNPlot = 2500;
+    if ~isempty(drift)
+        lowerLim = prctile(drift.Ta,2) - 20;
+        upperLim = prctile(drift.Ta,98) + 20;
+        
+        limTNPlot = nanmedian(drift.Tn);
+        
+        ax = subplot(3,2,1); plot(ax,drift.dateTime, drift.Tn, 'k'), hold on, plot(ax,drift.dateTime,drift.TSysLog,'y'), ylabel('Tn [K]') ,ylim([nanmedian(drift.Tn)-80,nanmedian(drift.Tn)+80])
+        plot(ax, [calibratedSpectra.meanAntTime], nanmedian(drift.Tn)*[calibratedSpectra.outlierCalib],'mx');
+        
+        ax2 = subplot(3,2,2); plot(ax2, drift.dateTime, drift.Ta ,'g'), hold on,ylabel('Ta [K]'),ylim([0,nanmedian(drift.Ta)+120])
+        plot(ax2, [calibratedSpectra.meanAntTime], nanmedian(drift.Ta)*[calibratedSpectra.outlierCalib],'mx');
+        if ~isempty(drift.outlierCold) && length(drift.outlierCold) < 100
+            plot(ax, drift.outlierCold,nanmedian(drift.Tn)-40,'bx')
+            plot(ax2, drift.outlierCold,10,'bx')
+        end
+        if ~isempty(drift.outlierHot) && length(drift.outlierHot) < 100
+            plot(ax, drift.outlierHot,nanmedian(drift.Tn)-50,'rx')
+            plot(ax2, drift.outlierHot,20,'rx')
+        end
+        if ~isempty(drift.outlierSky) && length(drift.outlierSky) < 100
+            plot(ax, drift.outlierSky,nanmedian(drift.Tn)-60,'gx')
+            plot(ax2, drift.outlierSky,30,'gx')
+        end
+        %plot(logFile.dateTime(drift.outlierSky),200*ones(length(drift.outlierSky)),'x')
+        %set(gca, 'ColorOrder', [1 0.5 0.5; 0.2 0.2 0.2, 0 0 1],'NextPlot', 'replacechildren');
+        colors = {'r','g','b'};
+        subplot(3,2,4);
+        for i=1:3
+            plot(drift.dateTime, drift.a(i,:),colors{i}), hold on, ylabel('Counts [-]'),ylim([nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200])
+        end
+        %     if ~isempty(drift.outlierCold)
+        %         for out = 1:length(drift.outlierCold)
+        %             line([drift.outlierCold(out) drift.outlierCold(out)],[nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200],'Color','b');
+        %         end
+        %     end
+        %     if ~isempty(drift.outlierSky)
+        %         for out = 1:length(drift.outlierSky)
+        %             line([drift.outlierSky(out) drift.outlierSky(out)],[nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200],'Color','g');
+        %         end
+        %     end
+        %     if ~isempty(drift.outlierHot)
+        %         for out = 1:length(drift.outlierHot)
+        %             line([drift.outlierHot(out) drift.outlierHot(out)],[nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200],'Color','r');
+        %         end
+        %     end
+        subplot(3,2,3); plot(drift.dateTime, drift.T, 'r'),  ylabel('T Hot  [K]'), ylim([mean(drift.T)-0.5 mean(drift.T)+0.5])
     end
-    if ~isempty(drift.outlierHot)
-        plot(ax, drift.outlierHot,nanmedian(drift.Tn)-50,'rx')
-        plot(ax2, drift.outlierHot,10,'rx')
+    if ~isempty(meteoData)
+        ax = subplot(3,2,5);
+        yyaxis(ax(1),'left')
+        plot(ax(1),[meteoData.dateTime], [meteoData.air_temperature] ,'r')
+        % set(ax(1),'ylim', [0,300])
+        %set(ax(1),'xlim', [0,24])
+        set(ax(1),'YColor','r');
+        ylabel(ax(1),({'airT [K]'}))
+        
+        yyaxis(ax(1),'right')
+        plot(ax(1),[meteoData.dateTime], [meteoData.air_pressure] ,'k')
+        %set(ax(1),'xlim', [0,24])
+        set(ax(1),'YColor','k');
+        ylabel(ax(1),({'airP [hPa]'}))
+        
+        ax2 = subplot(3,2,6);
+        yyaxis(ax2(1),'left')
+        plot(ax2(1), [meteoData.dateTime], [meteoData.precipitation] ,'k')
+        %set(ax2(1),'ylim', [0,300])
+        %set(ax2(1),'xlim', [0,24])
+        set(ax2(1),'YColor','k');
+        ylabel(ax2(1),({'precipitation [?]'}))
+        
+        yyaxis(ax2(1),'right')
+        plot(ax2(1),[meteoData.dateTime], [meteoData.rel_humidity] ,'b')
+        set(ax2(1),'ylim', [0,100])
+        %set(ax2(1),'xlim', [0,24])
+        set(ax2(1),'YColor','b');
+        ylabel(ax2(1),({'relH [%]'}))
     end
-    if ~isempty(drift.outlierSky)
-        plot(ax, drift.outlierSky,nanmedian(drift.Tn)-50,'gx')
-        plot(ax2, drift.outlierSky,10,'gx')
+    
+    %subplot(4,2,7); plot([calibratedSpectra.meanAntTime], [calibratedSpectra.flagged]);
+    
+    for i=1:6; subplot(3,2,i); grid on; end
+    print(fig,[calibrationTool.level1Folder calibrationTool.instrumentName '_calibratedSpectra_' calibrationTool.spectrometer '_' calibrationTool.dateStr],'-dpsc','-fillpage')
+    close
+    
+    fig2 = figure('visible','off');
+    %fig2 = figure();
+    clf
+    set(gcf, 'PaperPosition', [.1 .1 0.5, 0.5])
+    orient(fig2,'landscape')
+    cm = colormap(parula(N));
+    subplot(1,2,1);
+    for i=1:N
+        plot(calibratedSpectra(l(i)).if,calibratedSpectra(l(i)).Tb,'Color',cm(i,:));
+        %plot(calibratedSpectra(l(i)).freq,calibratedSpectra(l(i)).Tb);
+        
+        %plot(calibratedSpectra(l(i)).Tb)
+        TOD{i}=num2str(calibratedSpectra(l(i)).timeOfDay);
+        
+        %plot(calibratedSpectra(i).meanFromTbDownAll)
+        %plot(calibratedSpectra(l(i)).Tb-calibratedSpectra2(l(i)).Tb)
+        %xlabel('f [GHz]')
+        %xlim(1e-9*[calibratedSpectra(l(i)).freq(1),calibratedSpectra(l(i)).freq(end)])
+        ylabel('T_B [K]')
+        
+        ylim([lowerLim,upperLim])
+        %TOD{i}=num2str(calibratedSpectra(l(i)).timeOfDay);
+        hold on
     end
-    %plot(logFile.dateTime(drift.outlierSky),200*ones(length(drift.outlierSky)),'x')
-    %set(gca, 'ColorOrder', [1 0.5 0.5; 0.2 0.2 0.2, 0 0 1],'NextPlot', 'replacechildren');
-    colors = {'r','g','b'};
-    subplot(3,2,4); 
-    for i=1:3
-        plot(drift.dateTime, drift.a(i,:),colors{i}), hold on, ylabel('Counts [-]'),ylim([nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200])
+    
+    subplot(1,2,2);
+    for i=1:N
+        plot(calibratedSpectra(l(i)).if,calibratedSpectra(l(i)).TN,'Color',cm(i,:));
+        %plot(calibratedSpectra(l(i)).freq,calibratedSpectra(l(i)).T_rec);
+        ylabel('TN [K]')
+        ylim([limTNPlot-1000,limTNPlot+1000])
+        hold on
     end
-%     if ~isempty(drift.outlierCold)
-%         for out = 1:length(drift.outlierCold)
-%             line([drift.outlierCold(out) drift.outlierCold(out)],[nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200],'Color','b');
-%         end
-%     end
-%     if ~isempty(drift.outlierSky)
-%         for out = 1:length(drift.outlierSky)
-%             line([drift.outlierSky(out) drift.outlierSky(out)],[nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200],'Color','g');
-%         end
-%     end
-%     if ~isempty(drift.outlierHot)
-%         for out = 1:length(drift.outlierHot)
-%             line([drift.outlierHot(out) drift.outlierHot(out)],[nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200],'Color','r');
-%         end
-%     end
-    subplot(3,2,3); plot(drift.dateTime, drift.T, 'r'),  ylabel('T Hot  [K]'), ylim([mean(drift.T)-0.5 mean(drift.T)+0.5])
+    
+    
+    legend(TOD,'Location','southoutside','NumColumns',4)
+    for i=1:2; subplot(1,2,i); grid on, xlabel('IF [MHz]'); end
+    
+    
+    %legend(TOD)
+    %print([calibrationTool.level1Folder 'calibratedSpectra_' calibrationTool.dateStr '_' calibrationTool.spectrometer],'-dpdf','-fillpage')
+    print(fig2,[calibrationTool.level1Folder calibrationTool.instrumentName '_calibratedSpectra_' calibrationTool.spectrometer '_' calibrationTool.dateStr],'-dpsc','-append','-fillpage')
+    %saveas(gcf,[retrievalTool.level1Folder 'calibratedSpectra_' retrievalTool.dateStr '_' retrievalTool.spectrometer],'jpg')
+    close
+catch ME
+    warning(ME.identifier,'%s',['Plotting calibration problem: ' ME.message])
 end
-if ~isempty(meteoData)
-    ax = subplot(3,2,5);
-    yyaxis(ax(1),'left')
-    plot(ax(1),[meteoData.dateTime], [meteoData.air_temperature] ,'r')
-   % set(ax(1),'ylim', [0,300])
-    %set(ax(1),'xlim', [0,24])
-    set(ax(1),'YColor','r');
-    ylabel(ax(1),({'airT [K]'}))
-    
-    yyaxis(ax(1),'right')
-    plot(ax(1),[meteoData.dateTime], [meteoData.air_pressure] ,'k')
-    %set(ax(1),'xlim', [0,24])
-    set(ax(1),'YColor','k');
-    ylabel(ax(1),({'airP [hPa]'}))
-    
-    ax2 = subplot(3,2,6);
-    yyaxis(ax2(1),'left')
-    plot(ax2(1), [meteoData.dateTime], [meteoData.precipitation] ,'k')
-    %set(ax2(1),'ylim', [0,300])
-    %set(ax2(1),'xlim', [0,24])
-    set(ax2(1),'YColor','k');
-    ylabel(ax2(1),({'precipitation [?]'}))
-    
-    yyaxis(ax2(1),'right')
-    plot(ax2(1),[meteoData.dateTime], [meteoData.rel_humidity] ,'b')
-    set(ax2(1),'ylim', [0,100])
-    %set(ax2(1),'xlim', [0,24])
-    set(ax2(1),'YColor','b');
-    ylabel(ax2(1),({'relH [%]'}))
-end
-
-%subplot(4,2,7); plot([calibratedSpectra.meanAntTime], [calibratedSpectra.flagged]);
-
-for i=1:6; subplot(3,2,i); grid on; end
-print(fig,[calibrationTool.level1Folder calibrationTool.instrumentName '_calibratedSpectra_' calibrationTool.spectrometer '_' calibrationTool.dateStr],'-dpsc','-fillpage')
-close
-
-fig2 = figure('visible','off');
-clf
-set(gcf, 'PaperPosition', [.1 .1 0.5, 0.5])
-orient(fig2,'landscape')
-cm = colormap(parula(N));
-subplot(1,2,1);
-for i=1:N
-    plot(calibratedSpectra(l(i)).if,calibratedSpectra(l(i)).Tb,'Color',cm(i,:));
-    %plot(calibratedSpectra(l(i)).freq,calibratedSpectra(l(i)).Tb);
-    
-    %plot(calibratedSpectra(l(i)).Tb)
-    TOD{i}=num2str(calibratedSpectra(l(i)).timeOfDay);
-    
-    %plot(calibratedSpectra(i).meanFromTbDownAll)
-    %plot(calibratedSpectra(l(i)).Tb-calibratedSpectra2(l(i)).Tb)
-    %xlabel('f [GHz]')
-    %xlim(1e-9*[calibratedSpectra(l(i)).freq(1),calibratedSpectra(l(i)).freq(end)])
-    ylabel('T_B [K]')
-    ylim([lowerLim,upperLim])
-    %TOD{i}=num2str(calibratedSpectra(l(i)).timeOfDay);
-    hold on
-end
-
-subplot(1,2,2);
-for i=1:N
-    plot(calibratedSpectra(l(i)).if,calibratedSpectra(l(i)).TN,'Color',cm(i,:));
-    %plot(calibratedSpectra(l(i)).freq,calibratedSpectra(l(i)).T_rec);
-    ylabel('TN [K]')
-    ylim([100,5000])
-    hold on
-end
-
-
-legend(TOD,'Location','southoutside','NumColumns',4)
-for i=1:2; subplot(1,2,i); grid on, xlabel('IF [MHz]'); end
-
-
-%legend(TOD)
-%print([calibrationTool.level1Folder 'calibratedSpectra_' calibrationTool.dateStr '_' calibrationTool.spectrometer],'-dpdf','-fillpage')
-print(fig2,[calibrationTool.level1Folder calibrationTool.instrumentName '_calibratedSpectra_' calibrationTool.spectrometer '_' calibrationTool.dateStr],'-dpsc','-append','-fillpage')
-%saveas(gcf,[retrievalTool.level1Folder 'calibratedSpectra_' retrievalTool.dateStr '_' retrievalTool.spectrometer],'jpg')
-close
-
