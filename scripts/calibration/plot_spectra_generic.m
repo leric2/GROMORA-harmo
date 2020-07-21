e10f518a8a1121d5e7e72bdb8ec70445af3cb71a
Eric <eric.sauvageat@iap.unibe.ch>
2020-07-21 09:32:54 +0200

    same xlim on plot

--- scripts/calibration/plot_spectra_generic.m
+++ scripts/calibration/plot_spectra_generic.m
@@ -12,6 +12,9 @@ try
     set(gcf, 'PaperPosition', [1 1 19 27.7])
     orient(fig,'landscape')
     
+    xstart = calibrationTool.dateTime;
+    xstop = calibrationTool.dateTime + day(1);
+    
     limTNPlot = 2500;
     if ~isempty(drift)
         lowerLim = prctile(drift.Ta,2) - 20;
@@ -19,29 +22,29 @@ try
         
         limTNPlot = nanmedian(drift.Tn);
         
-        ax = subplot(3,2,1); plot(ax,drift.dateTime, drift.Tn, 'k'), hold on, plot(ax,drift.dateTime,drift.TSysLog,'y'), ylabel('Tn [K]') ,ylim([limTNPlot-200,limTNPlot+200])
-        plot(ax, [calibratedSpectra.meanAntTime], limTNPlot*[calibratedSpectra.outlierCalib]-100,'mx');
+        ax = subplot(3,2,1); plot(ax,drift.dateTime, drift.Tn, 'k'), hold on, plot(ax,drift.dateTime,drift.TSysLog,'y'), ylabel('Tn [K]') , xlim([xstart,xstop]), ylim([limTNPlot-200,limTNPlot+200])
+        plot(ax, [calibratedSpectra.meanAntTime], limTNPlot*[calibratedSpectra.outlierCalib]-100,'mx'), xlim([xstart,xstop]);
         
-        ax2 = subplot(3,2,2); plot(ax2, drift.dateTime, drift.Ta ,'g'), hold on,ylabel('Ta [K]'),ylim([0,nanmedian(drift.Ta)+120])
-        plot(ax2, [calibratedSpectra.meanAntTime], nanmedian(drift.Ta)*[calibratedSpectra.outlierCalib],'mx');
+        ax2 = subplot(3,2,2); plot(ax2, drift.dateTime, drift.Ta ,'g'), hold on,ylabel('Ta [K]'), xlim([xstart,xstop]), ylim([0,nanmedian(drift.Ta)+120])
+        plot(ax2, [calibratedSpectra.meanAntTime], nanmedian(drift.Ta)*[calibratedSpectra.outlierCalib],'mx'), xlim([xstart,xstop]);
         if ~isempty(drift.outlierCold) && length(drift.outlierCold) < 100
-            plot(ax, drift.outlierCold,nanmedian(drift.Tn)-40,'bx')
-            plot(ax2, drift.outlierCold,10,'bx')
+            plot(ax, drift.outlierCold,nanmedian(drift.Tn)-40,'bx'), xlim([xstart,xstop])
+            plot(ax2, drift.outlierCold,10,'bx'), xlim([xstart,xstop])
         end
         if ~isempty(drift.outlierHot) && length(drift.outlierHot) < 100
-            plot(ax, drift.outlierHot,nanmedian(drift.Tn)-50,'rx')
-            plot(ax2, drift.outlierHot,20,'rx')
+            plot(ax, drift.outlierHot,nanmedian(drift.Tn)-50,'rx'), xlim([xstart,xstop])
+            plot(ax2, drift.outlierHot,20,'rx'), xlim([xstart,xstop])
         end
         if ~isempty(drift.outlierSky) && length(drift.outlierSky) < 100
-            plot(ax, drift.outlierSky,nanmedian(drift.Tn)-60,'gx')
-            plot(ax2, drift.outlierSky,30,'gx')
+            plot(ax, drift.outlierSky,nanmedian(drift.Tn)-60,'gx'), xlim([xstart,xstop])
+            plot(ax2, drift.outlierSky,30,'gx'), xlim([xstart,xstop])
         end
         %plot(logFile.dateTime(drift.outlierSky),200*ones(length(drift.outlierSky)),'x')
         %set(gca, 'ColorOrder', [1 0.5 0.5; 0.2 0.2 0.2, 0 0 1],'NextPlot', 'replacechildren');
         colors = {'r','g','b'};
         subplot(3,2,4);
         for i=1:3
-            plot(drift.dateTime, drift.a(i,:),colors{i}), hold on, ylabel('Counts [-]'),ylim([prctile(drift.a(3,:),2)-200,prctile(drift.a(1,:),98)+200])
+            plot(drift.dateTime, drift.a(i,:),colors{i}), hold on, ylabel('Counts [-]'),ylim([prctile(drift.a(3,:),2)-200,prctile(drift.a(1,:),98)+200]), xlim([xstart,xstop])
         end
         %     if ~isempty(drift.outlierCold)
         %         for out = 1:length(drift.outlierCold)
@@ -58,20 +61,20 @@ try
         %             line([drift.outlierHot(out) drift.outlierHot(out)],[nanmedian(drift.a(3,:))-200,nanmedian(drift.a(1,:))+200],'Color','r');
         %         end
         %     end
-        subplot(3,2,3); plot(drift.dateTime, drift.T, 'r'),  ylabel('T Hot  [K]'), ylim([mean(drift.T)-0.5 mean(drift.T)+0.5])
+        subplot(3,2,3); plot(drift.dateTime, drift.T, 'r'),  ylabel('T Hot  [K]'),xlim([xstart,xstop]), ylim([mean(drift.T)-0.5 mean(drift.T)+0.5])
     end
     if ~isempty(fieldnames(meteoData))
         ax = subplot(3,2,5);
         yyaxis(ax(1),'left')
         plot(ax(1),[meteoData.dateTime], [meteoData.air_temperature] ,'r')
         % set(ax(1),'ylim', [0,300])
-        %set(ax(1),'xlim', [0,24])
+        set(ax(1),'xlim',[xstart,xstop])
         set(ax(1),'YColor','r');
         ylabel(ax(1),({'airT [K]'}))
         
         yyaxis(ax(1),'right')
         plot(ax(1),[meteoData.dateTime], [meteoData.air_pressure] ,'k')
-        %set(ax(1),'xlim', [0,24])
+        set(ax(1),'xlim',[xstart,xstop])
         set(ax(1),'YColor','k');
         ylabel(ax(1),({'airP [hPa]'}))
         
@@ -79,14 +82,14 @@ try
         yyaxis(ax2(1),'left')
         plot(ax2(1), [meteoData.dateTime], [meteoData.precipitation] ,'k')
         %set(ax2(1),'ylim', [0,300])
-        %set(ax2(1),'xlim', [0,24])
+        set(ax2(1),'xlim',[xstart,xstop])
         set(ax2(1),'YColor','k');
         ylabel(ax2(1),({'precipitation [?]'}))
         
         yyaxis(ax2(1),'right')
         plot(ax2(1),[meteoData.dateTime], [meteoData.rel_humidity] ,'b')
         set(ax2(1),'ylim', [0,100])
-        %set(ax2(1),'xlim', [0,24])
+        set(ax2(1),'xlim',[xstart,xstop])
         set(ax2(1),'YColor','b');
         ylabel(ax2(1),({'relH [%]'}))
     end
