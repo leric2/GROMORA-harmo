88a13b6d99b4ac0f0fe9cb59b51a95c8cd7f3930
Eric <eric.sauvageat@iap.unibe.ch>
2020-11-10 12:18:22 +0100

    new function to deal with level1b

--- scripts/calibration/compare_old_new.m
+++ scripts/calibration/compare_old_new.m
@@ -1,7 +1,52 @@
+figure()
+
+set(gcf,'color','w');
+set(gcf, 'Position',  [0, 0, 800, 600])
+%plot(drift.dailyMedianHotSpectra,'b','LineWidth',1)
+plot(level1b_filter.integration(3).if(level1b_filter.integration(3).potentialBadChannels==0), level1b_filter.integration(3).Tb(level1b_filter.integration(3).potentialBadChannels==0),'b','LineWidth',0.1)
+hold on
+plot(level1b_nofilter.integration(3).if(level1b_nofilter.integration(3).potentialBadChannels==0), level1b_nofilter.integration(3).Tb(level1b_filter.integration(3).potentialBadChannels==0),'r','LineWidth',0.1)
+
+grid on
+legend('flags filtering', 'no flag filtering', 'FontSize',12)
+xlabel('IF [MHz]', 'FontSize',14)
+xlim([0,1000])
+ylim([60,85])
+ylabel('T_B [K]', 'FontSize',14)
+title('MOPI5, AC240 integrated spectra from 16-Feb-2019 12:00-18:00', 'FontSize',14,'HorizontalAlignment', 'center')
+
+print([calibrationTool.level1Folder 'calSpectra_outleir_TN' calibrationTool.dateStr],'-dpdf')
+
+
+%%
+% plotting outlier examples
+figure()
+ind_outlier_hot=11348;
+
+set(gcf,'color','w');
+set(gcf, 'Position',  [0, 0, 800, 600])
+%plot(drift.dailyMedianHotSpectra,'b','LineWidth',1)
+plot(rawSpectra(ind_outlier_hot,:),'b','LineWidth',0.1)
+hold on
+plot(drift.dailyMedianHotSpectra+3*drift.dailyStdHotSpectra,'r','LineWidth',1)
+
+plot(drift.dailyMedianHotSpectra-3*drift.dailyStdHotSpectra,'r','LineWidth',1)
+
+grid on
+legend('outlier, 21-Feb-2019 11:23:02', 'daily median\pm3\sigma', 'FontSize',12)
+xlabel('Channels [-]', 'FontSize',14)
+xlim([0,16384])
+ylim([0,2.2*1e4])
+ylabel('FFT counts [-]', 'FontSize',14)
+%title('SOMORA, hot spectrum outlier from 21-Feb-2019', 'FontSize',16)
+
+print([calibrationTool.level1Folder 'outlier_hot_' calibrationTool.dateStr],'-dpdf')
+
+%%
 % plotting script, old and new routine
-load('/home/esauvageat/Documents/OG_GROMOS/meta3/level1/level1_dt_60min_2019_10_03.mat')
-somora_old_filename = '/home/esauvageat/Documents/SOMORA/SOMORA_RETRIEVAL/m20191003.bin';
-somora_old_filename_log = '/home/esauvageat/Documents/SOMORA/SOMORA_RETRIEVAL/r20191003.bin';
+load('/home/eric/Documents/PhD/GROSOM/Level1/level1_dt_60min_2019_10_03.mat')
+somora_old_filename = '/home/eric/Documents/PhD/GROSOM/Level1/m20191003.bin';
+somora_old_filename_log = '/home/eric/Documents/PhD/GROSOM/Level1/r20191003.bin';
 
 % 'GROMOS' // 'SOMORA' // 'mopi5' // 'MIAWARA-C'
 instrumentName='SOMORA';
@@ -47,7 +92,7 @@ calibrationTool.calibrationTime=10;
 
 % Total integration time
 calibrationTool.integrationTime=30;
-calibrationTool.minNumberOfAvgSpectra = 3;
+calibrationTool.minNumberOfAvgSpectra = 2;
 
 calibrationTool.filterByTransmittance = true;
 calibrationTool.filterByFlags = true;
@@ -128,35 +173,38 @@ center=1.4217504e+11;
 somora_frequencies=center-(8191*(1e+9/16384)):(1e+9/16384):center+(8192*(1e+9/16384));
 
 %%
-i = 40;
-figure()
-ax = subplot(2,1,1);
-
+i = 38;
+fig=figure()
+ax = subplot(1,2,1);
+set(gcf, 'Position',  [0, 0, 1100, 600])
+orient(fig,'landscape')
 set(gcf,'color','w');
 plot(somora_frequencies(105:end)/1e9,record(i).BT(105:end),'b','LineWidth',0.1)
 hold on
 plot(level1b_somora.integration(i).freq(level1b_somora.integration(i).potentialBadChannels==0)/1e9,level1b_somora.integration(i).Tb(level1b_somora.integration(i).potentialBadChannels==0),'r','LineWidth',0.1)
 
 grid on
-legend('old','new')
+legend('old','new','Location','northeast')
 xlabel('frequency [GHz]')
-xlim([141.6,142.8])
-ylim([median(record(i).BT(105:end))-10,median(record(i).BT(105:end))+35])
+%xlim([141.6,142.8])
+%ylim([median(record(i).BT(105:end))-10,median(record(i).BT(105:end))+25])
+ylim([95,130])
 ylabel('T_B [K]')
-title('SOMORA')
+title(['SOMORA : ',datestr(level1b_somora.integration(i).dateTime)])
 
 i=i/2;
-ax2 = subplot(2,1,2);
+ax2 = subplot(1,2,2);
 
 plot(ax2, level1b_gromos.integration(i).freq(level1b_gromos.integration(i).potentialBadChannels==0)/1e9,level1b_gromos.integration(i).Tb(level1b_gromos.integration(i).potentialBadChannels==0),'r','LineWidth',0.1)
 hold on
-plot(ax2, level1_data(i).frequencies/1e9,level1_data(i).bt,'b','LineWidth',1)
+plot(ax2, level1_data(i).frequencies/1e9,level1_data(i).bt,'b','LineWidth',0.6)
 grid on
-legend('new','old')
+%legend('new','old')
 xlabel('frequency [GHz]')
-xlim([141.6,142.8])
-ylim([median(level1_data(i).bt)-10,median(level1_data(i).bt)+40])
+%xlim([141.6,142.8])
+ylim([95,130])
+%ylim([median(level1_data(i).bt)-10,median(level1_data(i).bt)+25])
 ylabel('T_B [K]')
-title('GROMOS')
+title(['GROMOS : ',datestr(level1b_gromos.integration(i).dateTime)])
 
-print([calibrationTool.level1Folder 'comparison_GROMOS_SOMORA_77K' calibrationTool.dateStr],'-dpdf','-fillpage')
+print([calibrationTool.level1Folder 'comparison_GROMOS_SOMORA_77K' calibrationTool.dateStr],'-dpdf')
--- scripts/calibration/read_level1b_daily.m
+++ scripts/calibration/read_level1b_daily.m
@@ -1,4 +1,4 @@
-function [calib, meteoData, calibrationTool] = read_level1b_daily(calibrationTool)
+function [data, meteoData, calibrationTool] = read_level1b_daily(calibrationTool)
 %==========================================================================
 % NAME          | read_level1a_daily.m
 % TYPE          | function
@@ -26,121 +26,77 @@ function [calib, meteoData, calibrationTool] = read_level1b_daily(calibrationToo
 filename=calibrationTool.filenameLevel1b;
 
 ncinfo(filename)
-ncinfo(filename).Groups(1).Name
-ncinfo(filename).Groups(1).Variables.Name
-
-for g=1:length(ncinfo(filename).Groups):
-    group = ncinfo(filename).Groups(g);
-    groupName = group.Name;
-    
-    for v=1:length(group.Variables)
-        varName = group.Variables(v).Name
-        ncread(filename,[+groupName+'/ '+varName])
+gNames = {ncinfo(filename).Groups.Name};
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Read the different dataset and variables
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Coordinate variables, directly adding the attributes
-correctedSpectra.meanTime = ncread(filename,'/spectrometer1/time')';
-correctedSpectra.timeUnit = ncreadatt(filename,'/spectrometer1/time','units');
-correctedSpectra.timeCalendar= ncreadatt(filename,'/spectrometer1/time','calendar');
-
-correctedSpectra.meanDateTime = datetime(correctedSpectra.meanTime+datenum(1970,1,1),'ConvertFrom','datenum');
-
-correctedSpectra.channelID = ncread(filename,'/spectrometer1/channel_idx')';
-
-% some variable for better identifying the time period of the measurements
-correctedSpectra.year=ncread(filename,'/spectrometer1/year');
-correctedSpectra.month= ncread(filename,'/spectrometer1/month');
-correctedSpectra.day = ncread(filename,'/spectrometer1/day');
-% ncread(filename,'/spectrometer1/timeOfDay',[calibratedSpectra.timeOfDay]);
-
-correctedSpectra.firstSkyTime=ncread(filename,'/spectrometer1/first_sky_time')';
-% ncreadatt(filename,'/spectrometer1/firstSkyTime','units',calibratedSpectra(1).meanDatetimeUnit);
-% ncreadatt(filename,'/spectrometer1/firstSkyTime','calendar',calibratedSpectra(1).calendar);
-% ncreadatt(filename,'/spectrometer1/firstSkyTime','description','start time of the first sky measurements in this cycle');
-
-correctedSpectra.lastSkyTime=ncread(filename,'/spectrometer1/last_sky_time')';
-% ncreadatt(filename,'/spectrometer1/lastSkyTime','units',calibratedSpectra(1).meanDatetimeUnit);
-% ncreadatt(filename,'/spectrometer1/lastSkyTime','calendar',calibratedSpectra(1).calendar);
-% ncreadatt(filename,'/spectrometer1/lastSkyTime','description','stop time of the first sky measurements in this cycle');
-
-correctedSpectra.timeMin=ncread(filename,'/spectrometer1/time_min')';
-correctedSpectra.tod = ncread(filename,'/spectrometer1/time_of_day')';
-
-% TO IMPLEMENT, NEEDED ? 
-% function vec = split_vec(v)
-% vec=num2cell(v);
-% vec=a{:};
-% end
-% 
-% split_vec(ncread(filename,'/spectrometer1/THot'))
-
-% % Scientific Dataset (spectrometer1,2,...)
-% Calibration variable
-correctedSpectra.Tb=ncread(filename,'/spectrometer1/Tb')';
-correctedSpectra.stdTb=ncread(filename,'/spectrometer1/stdTb')';
-correctedSpectra.freq=ncread(filename,'/spectrometer1/frequencies')';
-correctedSpectra.if=ncread(filename,'/spectrometer1/intermediate_freq')';
-correctedSpectra.THot=ncread(filename,'/spectrometer1/THot')';
-correctedSpectra.stdTHot=ncread(filename,'/spectrometer1/stdTHot')';
-correctedSpectra.TSys=ncread(filename,'/spectrometer1/TSys')';
-correctedSpectra.stdTSys=ncread(filename,'/spectrometer1/stdTSys')';
-correctedSpectra.meanStdTb=ncread(filename,'/spectrometer1/mean_std_Tb')';
-correctedSpectra.meanAngleAntenna=ncread(filename,'/spectrometer1/mean_sky_elevation_angle')';
-correctedSpectra.numHotSpectra=ncread(filename,'/spectrometer1/number_of_hot_spectra')';
-correctedSpectra.numColdSpectra=ncread(filename,'/spectrometer1/number_of_cold_spectra')';
-correctedSpectra.numAntSpectra=ncread(filename,'/spectrometer1/number_of_sky_spectra')';
-correctedSpectra.TWindow=ncread(filename,'/spectrometer1/TWindow')';
-correctedSpectra.calibrationTime=ncread(filename,'/spectrometer1/calibration_time')';
-try
-    correctedSpectra.noiseLevel=ncread(filename,'/spectrometer1/noise_level')';
-catch ME
-     warning(ME.identifier,': no noise level defined for calibration');
-     correctedSpectra.noiseLevel = -9999*ones(1,length(correctedSpectra.meanTime));
-end
-%correctedSpectra.effectiveCalibrationTime=ncread(filename,'/spectrometer1/effectiveCalibrationTime')';
-
-% ncread(filename,'/spectrometer1/stdTHot',[calibratedSpectra.stdTHot]);
-% ncread(filename,'/spectrometer1/TSys',[calibratedSpectra.Tsys]);
-% ncread(filename,'/spectrometer1/stdTSys',[calibratedSpectra.stdTSys]);
-% ncread(filename,'/spectrometer1/calibrationTime',[calibratedSpectra.calibrationTime]);
-% ncread(filename,'/spectrometer1/meanAngleAntenna',[calibratedSpectra.meanAngleAntenna]);
-%
-% ncread(filename,'/spectrometer1/TRoom',[calibratedSpectra.TempRoom]);
-% ncread(filename,'/spectrometer1/stdTRoom',[calibratedSpectra.stdTempRoom]);
-% ncread(filename,'/spectrometer1/TOut',[calibratedSpectra.TempOut]);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Reading the flags variables
-correctedSpectra.flagVector=ncread(filename,'/flags/calibration_flags')';
-calibrationTool.flagVectorLength = ncreadatt(filename,'/flags/','number_of_flags');
-calibrationTool.logFile.calibration_flags_meaning = [];
-for i = 1:calibrationTool.flagVectorLength
-    varName = ['errorCode_' num2str(i)];
-    errorCodeMeaning = ncreadatt(filename,'/flags/calibration_flags',varName);
-    calibrationTool.logFile.calibration_flags_meaning = [calibrationTool.logFile.calibration_flags_meaning, string(errorCodeMeaning)];
+data = struct();
+meteoData= struct();
+for g=1:length(gNames)
+    gName = gNames{g};
+    
+    if strcmp(gName,'meteo')
+        vNames = {ncinfo(filename,gName).Variables.Name};
+        %dimNames = {ncinfo(filename,gName).Dimensions.Name};
+        %         size=zeros(1,length(dimNames));
+        %         for i=1:length(dimNames)
+        %             size(i) = ncinfo(filename,fullfile(gName,dimNames{i})).Size;
+        %         end
+        
+        for v=1:length(vNames)
+            varName = vNames{v};
+            
+            if length(ncinfo(filename,fullfile(gName,varName)).Dimensions)==1
+                if strcmp(ncinfo(filename,fullfile(gName,varName)).Dimensions.Name,'time')
+                    meteoData.(varName) = ncread(filename,fullfile(gName,varName))';
+                else
+                    meteoData(t).(varName) = ncread(filename,fullfile(gName,varName),1,Inf);
+                end
+            else
+                meteoData(t).(varName)= ncread(filename,fullfile(gName,varName),[1,t],[Inf,1])';
+            end
+        end
+    else
+        vNames = {ncinfo(filename,gName).Variables.Name};
+        dimNames = {ncinfo(filename,gName).Dimensions.Name};
+        size=zeros(1,length(dimNames));
+        for i=1:length(dimNames)
+            size(i) = ncinfo(filename,fullfile(gName,dimNames{i})).Size;
+        end
+        
+        % Intialize our structure with time
+        for t = 1:size(1)
+            varName = vNames{1};
+            data(t).(varName) = ncread(filename,fullfile(gName,varName),t,1);
+        end
+        dateT = num2cell(datetime([data.time] + datenum(1970,1,1),'ConvertFrom','datenum'));
+        [data.meanDateTime] = dateT{:};
+        %if strcmp('time',dimName)
+        for v=2:length(vNames)
+            varName = vNames{v};
+            
+            if length(ncinfo(filename,fullfile(gName,varName)).Dimensions)==1
+                if strcmp(ncinfo(filename,fullfile(gName,varName)).Dimensions.Name,'time')
+                    dat=num2cell(ncread(filename,fullfile(gName,varName)));
+                    [data.(varName)] = dat{:};
+                else
+                    for t = 1:size(1)
+                        data(t).(varName) = ncread(filename,fullfile(gName,varName),1,Inf)';
+                    end
+                end
+            else
+                %dat=num2cell(ncread(filename,fullfile(gName,varName)))';
+                for t = 1:size(1)
+                    data(t).(varName)= ncread(filename,fullfile(gName,varName),[1,t],[Inf,1])';
+                end
+            end
+        end
+    end
 end
 
-% calibrationTool.logFile.calibration_flags_meaning = [
-%     string(ncreadatt(filename,'/flags/calibration_flags','errorCode_1')),...
-%     string(ncreadatt(filename,'/flags/calibration_flags','errorCode_2')),...
-%     string(ncreadatt(filename,'/flags/calibration_flags','errorCode_3')),...
-%     string(ncreadatt(filename,'/flags/calibration_flags','errorCode_4')),...
-%     string(ncreadatt(filename,'/flags/calibration_flags','errorCode_5')),...
-%     string(ncreadatt(filename,'/flags/calibration_flags','errorCode_6'))];
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Reading the meteo variables
-meteoData.dateNum = ncread(filename,'/meteo/time')';
-meteoData.dateTime = datetime(meteoData.dateNum + datenum(1970,1,1),'ConvertFrom','datenum');
-meteoData.air_pressure = ncread(filename,'/meteo/air_pressure')';
-meteoData.air_temperature = ncread(filename,'/meteo/air_temperature')';
-meteoData.rel_humidity = ncread(filename,'/meteo/relative_humidity')';
-meteoData.precipitation = ncread(filename,'/meteo/precipitation')';
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Reading global attributes
+% Reading attributes
 calibrationTool.logFile.raw_file_warning=ncreadatt(filename,'/','raw_file_warning');
 calibrationTool.logFile.comment=ncreadatt(filename,'/','comment');
 calibrationTool.logFile.raw_file_comment=ncreadatt(filename,'/','raw_file_comment');
@@ -152,35 +108,22 @@ calibrationTool.logFile.creation_date_level1a=ncreadatt(filename,'/','creation_d
 calibrationTool.logFile.raw_data_software_version=ncreadatt(filename,'/','raw_data_software_version');
 calibrationTool.logFile.filenameLevel1a=ncreadatt(filename,'/','filename');
 
-disp(['File read : ' filename])
+calibrationTool.flagVectorLength = ncreadatt(filename,'/flags/','number_of_flags');
 
-for i = 1:length(correctedSpectra.meanTime)
-    calib(i).Tb = correctedSpectra.Tb(i,:);
-    calib(i).stdTb = correctedSpectra.stdTb(i,:);
-    calib(i).dateTime = correctedSpectra.meanDateTime(i);
-    calib(i).freq = correctedSpectra.freq(:)';
-    calib(i).if = correctedSpectra.if(:)';
-    calib(i).year = correctedSpectra.year(i);
-    calib(i).month = correctedSpectra.month(i);
-    calib(i).day = correctedSpectra.day(i);
-    calib(i).THot = correctedSpectra.THot(i);
-    calib(i).stdTHot = correctedSpectra.stdTHot(i);
-    calib(i).TSys = correctedSpectra.TSys(i);
-    calib(i).TWindow = correctedSpectra.TWindow(i);
-    calib(i).meanStdTb = correctedSpectra.meanStdTb(i);
-    calib(i).meanAngleAntenna  =  correctedSpectra.meanAngleAntenna(i);
-    calib(i).numHotSpectra  =  correctedSpectra.numHotSpectra(i);
-    calib(i).numColdSpectra  =  correctedSpectra.numColdSpectra(i);
-    calib(i).numAntSpectra  =  correctedSpectra.numAntSpectra(i);
-    calib(i).stdTSys = correctedSpectra.stdTSys(i);
-    calib(i).calibrationTime = correctedSpectra.calibrationTime(i);
-    calib(i).firstSkyTime = correctedSpectra.firstSkyTime(i);
-    calib(i).lastSkyTime = correctedSpectra.lastSkyTime(i);
-    calib(i).timeMin = correctedSpectra.timeMin(i);
-    calib(i).TOD = correctedSpectra.tod(i);
-    calib(i).noiseLevel = correctedSpectra.noiseLevel(i);
-    calib(i).flags = correctedSpectra.flagVector(i,:);
+% Coordinate variables, directly adding the attributes
+calibrationTool.timeUnit = ncreadatt(filename,'/spectrometer1/time','units');
+calibrationTool.timeCalendar= ncreadatt(filename,'/spectrometer1/time','calendar');
+
+
+meteoData.dateTime = datetime(meteoData.time + datenum(1970,1,1),'ConvertFrom','datenum');
+
+if ~isfield(data,'noise_level')
+     warning('no noise level defined for calibration');
+     in = num2cell(-9999*ones(1,length(data)));
+     [data.noiseLevel] = in{:};
 end
+
+disp(['File read : ' filename])
 
 end
 
