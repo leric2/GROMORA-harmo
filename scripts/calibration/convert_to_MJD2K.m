8d0af659f0fb6e43477c1cbcac8454a6219ec2ce
Eric <eric.sauvageat@iap.unibe.ch>
2020-12-04 17:57:05 +0100

    new function to compute MJD2K

---
+++ scripts/calibration/convert_to_MJD2K.m
@@ -0,0 +1,40 @@
+function mjd2k_date = convert_to_MJD2K(YYYY, MM, DD, hh, mm, ss)
+%==========================================================================
+% NAME      | convert_to_MJD2K(YYYY, MM, DD, hh, mm, ss)
+% TYPE      | Function
+% AUTHOR(S) | Eric Sauvageat
+% CREATION  | 12.2020
+%           |
+% ABSTRACT  | Function to compute the MJD2K time. Taken from GEOMS report 1.0:
+%           | https://avdc.gsfc.nasa.gov/PDF/GEOMS/geoms-1.0.pdf
+%           |
+%           |
+%           |
+% ARGUMENTS | INPUTS: YYYY,MM, DD, hh, mm, ss --> UTC !
+%           |
+%           | OUTPUTS: - mjd2k_date: a 
+%           | 
+%==========================================================================
+
+if MM>2
+    y = double(YYYY);
+    m = double(MM-3);
+    d = double(DD);
+else
+    y = double(YYYY-1);
+    m = double(MM + 9);
+    d = double(DD);
+end
+
+j = round(365.25*(y+4712)) + round(30.6*m+0.5) + 58.5 + d;
+
+if j < 2299159.5
+    jd = j;
+else
+    gn = 38 - round(3*round(49 + y/100)/4);
+    jd = j+gn;
+end
+
+df = (hh*3600.0 + mm*60.0 + ss)/86400;
+
+mjd2k_date = jd + df - 2451544.5;
