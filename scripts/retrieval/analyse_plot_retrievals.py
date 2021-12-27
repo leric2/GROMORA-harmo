d81389b9a2af02adf0ca41336b3955235939ffe4
Eric <eric.sauvageat@iap.unibe.ch>
2021-12-27 10:41:58 +0100

    minor plots changes

--- scripts/retrieval/analyse_plot_retrievals.py
+++ scripts/retrieval/analyse_plot_retrievals.py
@@ -70,7 +70,7 @@ instrument_name = "GROMOS"
 # lowerBound = [0, 95, 100, 110, 120, 130, 140, 180]
 
 # date = pd.date_range(start='2019-01-30', end='219-06-18')
-date = pd.date_range(start='2017-01-01', end='2017-12-31')
+date = pd.date_range(start='2020-12-19', end='2020-12-20')
 #date = pd.date_range(start='2017-09-01', end='2018-01-05')
 #date = datetime.date(2016,1,2)
 #date = [datetime.date(2019,3,11), datetime.date(2019,4,3)]
@@ -83,7 +83,7 @@ df_bins = 200e3
 plot_all = False
 plot_all_mopi5 = False
 plot_o3_ts = True
-save_o3= True
+save_o3= False
 save_o3_only = False
 save_residuals=False
 plot_selected = False
@@ -97,9 +97,12 @@ compare = False
 plot_polyfit = False
 plot_MLS = False
 
+add_opacity=False
+
 extract_fgrid = False
 
 filename_opacity = '/scratch/GROSOM/Level2/opacities/'+instrument_name+'/'+instrument_name+'_opacity_'+str(date[0].year)+'.nc'
+#  filename_opacity_new = '/scratch/GROSOM/Level1/'+instrument_name+'_level1b_v2_all.nc'
 
 compare_MERRA2 = False
 compare_ECMWF = False
@@ -127,6 +130,7 @@ ex = '_waccm_monthly_scaled_h2o'
 ex = '_gromosAP_scaled_h2o'
 ex = '_waccm_low_alt_dx10_nonWinCorr'
 ex = '_gromosAP_low_alt'
+ex = '_waccm_low_alt_RFI'
 # %%
 
 colormap = 'cividis'  # 'viridis' #, batlow_map cmap_crameri cividis
@@ -142,6 +146,15 @@ def read_opacity(filename, date):
 
     ds_opacity = ds_opacity.sel(time=slice(date[0].strftime('%Y-%m-%d'), date[-1].strftime('%Y-%m-%d')))
     return ds_opacity
+
+def read_level1b(filename):
+    ds = xr.open_dataset(
+        filename,
+        decode_times=True,
+        decode_coords=True,
+        # use_cftime=True,
+    )
+    return ds
 
 def read_mls(d1, d2):
     MLS_basename = '/home/esauvageat/Documents/AuraMLS/'
@@ -275,7 +288,8 @@ if extract_fgrid:
     no_flag=True, meta_data=False)
     integrated_data['AC240'].isel(time=0).frequencies.to_netcdf('/scratch/GROSOM/Level1/frequency_grid.nc')
 if plot_cost:
-    ds_opacity = read_opacity(filename_opacity, date)
+    if add_opacity:
+        ds_opacity = read_opacity(filename_opacity, date)
 
     if instrument_name == 'mopi5':
         for s in spectros:
@@ -291,7 +305,8 @@ if plot_cost:
             noise_input = level2_dataset[s].median_noise
             end_cost.plot(marker='.', ax=axes[0])
             noise_input.plot(marker='.',ax=axes[1])
-            ds_opacity.tropospheric_opacity.plot(marker='.',ax=axes[2])
+            if add_opacity:
+                ds_opacity.tropospheric_opacity.plot(marker='.',ax=axes[2])
             #axs[1].set_ylim(0,1)
             axes[0].set_xticks([])
             axes[1].set_xticks([])
@@ -301,7 +316,8 @@ if plot_cost:
 
 
 if plot_polyfit:
-    ds_opacity = read_opacity(filename_opacity, date)
+    if add_opacity:
+        ds_opacity = read_opacity(filename_opacity, date)
 
     if instrument_name=='compare':
         s = 'AC240'
@@ -325,10 +341,12 @@ if plot_polyfit:
         for i in [0,1,2,3]:
             axes[i].set_xlabel('')
             axes[i].set_xticks([])
-        ds_opacity.tropospheric_opacity.plot(marker='.',ax=axes[4], color='k')
-        axes[4].set_ylabel('opacity [-]')
-        axes[4].set_xlabel('')
-        axes[4].set_title('Tropospheric opacity (Ingold)')
+        
+        if add_opacity:
+            ds_opacity.tropospheric_opacity.plot(marker='.',ax=axes[4], color='k')
+            axes[4].set_ylabel('opacity [-]')
+            axes[4].set_xlabel('')
+            axes[4].set_title('Tropospheric opacity (Ingold)')
       #  perc=100*(polyfit_1[0]-polyfit_2[0])/polyfit_1[0]
        # perc.plot(marker='.', ax=axes[3])
         axes[0].legend(('SOMORA', 'GROMOS')) 
@@ -354,7 +372,8 @@ if plot_polyfit:
             for i in [0,1,2,3]:
                 axes[i].set_xlabel('')
                 axes[i].set_xticks([])
-            ds_opacity.tropospheric_opacity.plot(marker='.',ax=axes[4], color='k')
+            if add_opacity:
+                ds_opacity.tropospheric_opacity.plot(marker='.',ax=axes[4], color='k')
             #ds_opacity.tropospheric_opacity_tc.plot(marker='.',ax=axes[3])
             #axes[3].set_ylim(0,20)
             #axs[1].set_ylim(0,1)
@@ -738,7 +757,7 @@ if plot_selected_nicer:
         level2_dataset,
         outname,
         spectro='AC240',
-        cycles=[0]#cycles=[1,7,10,13,17,21]
+        cycles=[1, 10, 17]#cycles=[1,7,10,13,17,21]
     )
 
 
